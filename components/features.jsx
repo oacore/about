27ad2323df6e1e2b42c98841f29050c35e401ab8
Viktor Yakubiv <viktor@yakubiv.com>
2018-11-14 18:38:09 +0200

    implement features component

---
+++ components/features.jsx
@@ -0,0 +1,103 @@
+import React from 'react'
+import {
+  Nav,
+  NavItem,
+  NavLink,
+  Fade,
+  Row,
+  Col,
+} from 'reactstrap'
+
+
+const Feature = ({ children, isActive }) => (
+  <Fade in={isActive} hidden={!isActive}>
+    {children}
+  </Fade>
+)
+
+class FeaturesSection extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      activePane: null
+    }
+  }
+
+  static getDerivedStateFromProps({ children }, state) {
+    if (state.activePane !== null) return state
+
+    let firstPane = null
+    React.Children.forEach(children, (child) => {
+      if (!firstPane && child.type === Feature) firstPane = child
+    })
+
+    return {
+      ...state,
+      activePane: firstPane.props.id,
+    }
+  }
+
+  toggleTab(tabId) {
+    this.setState({ activePane: tabId })
+  }
+
+  render() {
+    const { activePane: activePaneId } = this.state
+
+    const beforeTabs = []
+    const afterTabs = []
+    const tabs = []
+
+    // Filter features to 3 lists
+    React.Children.forEach(this.props.children, (child) => {
+      if (child.type === Feature) tabs.push(child)
+      else (tabs.length == 0 ? beforeTabs : afterTabs).push(child)
+    })
+
+    const tabPanes = tabs.map(tab => React.cloneElement(tab, {
+      isActive: activePaneId === tab.props.id,
+    }))
+
+    const tabPills = tabPanes.map((tab) => {
+      const { forTitle, id } = tab.props
+      return (
+        <NavItem key={id}>
+          <NavLink
+            active={id === activePaneId}
+            href={`#${id}`}
+            onClick={this.toggleTab.bind(this, id)}
+          >
+            CORE for <b>{forTitle}</b>
+          </NavLink>
+        </NavItem>
+      )
+    })
+
+    return (
+      <section id="features" className="features">
+        {beforeTabs}
+        <Row>
+          <Col xs="12" md="3">
+            <Nav
+              vertical
+              pills
+              role="tablist"
+              aria-orientation="vertical"
+            >{tabPills}</Nav>
+          </Col>
+          <Col xs="12" md="9">
+            <div className="tab-content">{tabPanes}</div>
+          </Col>
+        </Row>
+        {afterTabs}
+      </section>
+    )
+  }
+}
+
+
+export {
+  FeaturesSection as Features,
+  Feature,
+}
