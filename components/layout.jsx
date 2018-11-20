ee99b2af95b294168934915affc6b5ae7ea534f6
Viktor Yakubiv <viktor@yakubiv.com>
2018-11-20 17:40:27 +0200

    convert sitemap to global config
    - add footer section
    - add general meta
    - use meta in head
    
    go forward FRONTEND-7 (JIRA)

--- components/layout.jsx
+++ components/layout.jsx
@@ -5,7 +5,7 @@ import Head from 'next/head'
 import Header from './header'
 import Footer from './footer'
 
-import siteMap from '../sitemap.yml'
+import config from '../core.config.yml'
 
 const Layout = ({ children, container = false }) => (
   <React.Fragment>
@@ -14,9 +14,11 @@ const Layout = ({ children, container = false }) => (
         name="viewport"
         content="width=device-width, initial-scale=1, shrink-to-fit=no"
       />
-      <title>CORE</title>
+      <title>{config.title}</title>
+      <meta name="description" content={config.description} />
     </Head>
-    <Header siteMap={siteMap} />
+
+    <Header siteMap={config.navigation} />
     {container ? <Container>{children}</Container> : children}
     <Footer />
   </React.Fragment>
