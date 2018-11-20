a06617d56586866764eb065634ba561837d2717b
Viktor Yakubiv <viktor@yakubiv.com>
2018-11-20 17:19:12 +0200

    populate simple head
    - add support of mobile phones
    - add simple title

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Container } from 'reactstrap'
+import Head from 'next/head'
 
 import Header from './header'
 import Footer from './footer'
@@ -8,6 +9,13 @@ import siteMap from '../sitemap.yml'
 
 const Layout = ({ children, container = false }) => (
   <React.Fragment>
+    <Head>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+      />
+      <title>CORE</title>
+    </Head>
     <Header siteMap={siteMap} />
     {container ? <Container>{children}</Container> : children}
     <Footer />
