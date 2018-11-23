b31e0faced0777bdfe9c3efd5aa59634bd6f7c90
Viktor Yakubiv <viktor@yakubiv.com>
2018-11-23 17:14:41 +0200

    add key features to home page
    resolve FRONTEND-16 (JIRA)

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,20 @@
 import React from 'react'
-import { Layout, Hero } from '../components'
+import { Container } from 'reactstrap'
+import NextLink from 'next/link'
+
+import { Layout, Hero, KeyFeature, KeyFeatureList } from '../components'
+
+import apiIcon from '../images/api.svg'
+import globeIcon from '../images/globe.svg'
+import hallIcon from '../images/hall.svg'
+
+/* eslint-disable jsx-a11y/anchor-is-valid */
+const Link = ({ children, ...args }) => (
+  <NextLink {...args}>
+    <a>{children}</a>
+  </NextLink>
+)
+/* eslint-enable jsx-a11y/anchor-is-valid */
 
 const IndexPage = () => (
   <Layout>
@@ -7,6 +22,27 @@ const IndexPage = () => (
       Seamless access to the worldʼs biggest collection of open access research
       papers
     </Hero>
+
+    <Container>
+      <KeyFeatureList className="my-5">
+        <KeyFeature title="Worldwide data" icon={globeIcon}>
+          We aggregate and enrich open access research papers from around the
+          world
+          <br />
+          <Link href="/services/data">Read about our data</Link>
+        </KeyFeature>
+        <KeyFeature title="Unique APIs" icon={apiIcon}>
+          We provide seamless access to content and data, through our
+          unique&nbsp;<Link href="/services/api">APIs</Link>
+          <br />
+          <Link href="/services/api">Perfect for text mining!</Link>
+        </KeyFeature>
+        <KeyFeature title="Powerful Services" icon={hallIcon}>
+          We create powerful <Link href="/services">services</Link> for
+          researchers, universities, and industry
+        </KeyFeature>
+      </KeyFeatureList>
+    </Container>
   </Layout>
 )
 
