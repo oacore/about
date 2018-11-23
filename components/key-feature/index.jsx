fd04d77f2cf350869c374d7e3693f0acd3b7fd00
Viktor Yakubiv <viktor@yakubiv.com>
2018-11-23 17:10:34 +0200

    implement key-feature component
    moving out from Bootstrap
    
    go ahead FRONTEND-16 (JIRA)

---
+++ components/key-feature/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+import './key-feature.scss'
+
+const KeyFeature = ({
+  title,
+  children,
+  icon,
+  className,
+  tag: Tag = 'div',
+  ...args
+}) => (
+  <Tag className={`key-feature ${className}`} {...args}>
+    <img className="key-feature-icon" src={icon} alt={title} />
+    <p className="key-feature-text">{children}</p>
+  </Tag>
+)
+
+const KeyFeatureList = ({ children, className }) => {
+  const items = React.Children.map(children, child => {
+    if (child.type !== KeyFeature) return null
+
+    return React.cloneElement(child, {
+      tag: 'li',
+      key: child.props.title,
+    })
+  })
+
+  return <ul className={`key-feature-list ${className}`}>{items}</ul>
+}
+
+KeyFeatureList.Item = KeyFeature
+
+export default KeyFeature
+export { KeyFeature, KeyFeatureList }
