4eb3226e881430b0e916a81c91542e48e84c18d1
Viktor Yakubiv <viktor@yakubiv.com>
2018-11-23 15:11:53 +0200

    improve hero background

--- components/hero/index.jsx
+++ components/hero/index.jsx
@@ -5,22 +5,19 @@ import SearchForm from '../search'
 
 import './hero.scss'
 
-const Hero = ({ children, includeSearch = true }) => {
-  const accent = (Math.ceil(Math.random() * 1000) % 3) + 1
-  return (
-    <div className={`hero hero-accent-${accent}`}>
-      <Container>
-        <div className="mx-auto col-md-6">
-          <Logo />
-          <p className="lead text-center">{children}</p>
-        </div>
+const Hero = ({ children, includeSearch = true }) => (
+  <div className="hero">
+    <Container>
+      <div className="mx-auto col-md-6">
+        <Logo />
+        <p className="lead text-center">{children}</p>
+      </div>
 
-        {includeSearch && (
-          <SearchForm placeholder="Search over 100,000,000 articles" />
-        )}
-      </Container>
-    </div>
-  )
-}
+      {includeSearch && (
+        <SearchForm placeholder="Search over 100,000,000 articles" />
+      )}
+    </Container>
+  </div>
+)
 
 export default Hero
