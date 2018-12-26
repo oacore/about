3ab459c3cbfa17d605870e3d4b334e62af0fcba5
Viktor Yakubiv <viktor@yakubiv.com>
2018-12-26 18:59:42 +0200

    change URL when open FAQ

--- pages/faq.jsx
+++ pages/faq.jsx
@@ -3,13 +3,19 @@ import { Article, Section, Accordion, Content } from '../components'
 
 import faqData from '../data/faq.yml'
 
+const itemToURL = id => {
+  const url = new URL(window.location)
+  url.hash = id ? `#${id}` : ''
+  window.history.replaceState({}, null, url.toString())
+}
+
 const FAQsPage = () => (
   <Article container>
     <h1>{faqData.title}</h1>
     {faqData.sections.map(section => (
       <Section caption={section.title} key={section.title}>
         <h2>{section.title}</h2>
-        <Accordion>
+        <Accordion onToggle={itemToURL}>
           {section.items.map(({ slug, question, answer }) => (
             <Accordion.Item id={slug} title={question} key={slug}>
               <Content markdown>{answer}</Content>
