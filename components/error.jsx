09ae0bbc84cfff48598ef10f46447467ca65895f
Viktor Yakubiv <viktor@yakubiv.com>
2018-12-26 16:35:28 +0200

    implement custom error component

---
+++ components/error.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { Button } from 'reactstrap'
+import { Article } from './content'
+import Link from './link'
+
+class ErrorPage extends Component {
+  static getInitialProps({ res, err }) {
+    // eslint-disable-next-line no-nested-ternary
+    const statusCode = res ? res.statusCode : err ? err.statusCode : null
+    return { statusCode }
+  }
+
+  render() {
+    return (
+      <Article container>
+        <h1>Uh-oh</h1>
+        <p>The page you were looking for could not be found.</p>
+        <p>To help you find what you are looking for, why not</p>
+        <p>
+          <Link href="~home" passHref>
+            <Button color="primary">Go back to the homepage</Button>
+          </Link>
+          {' or '}
+          <Link href="~contact" passHref>
+            <Button color="primary" outline>
+              Contact us
+            </Button>
+          </Link>
+        </p>
+      </Article>
+    )
+  }
+}
+
+export default ErrorPage
