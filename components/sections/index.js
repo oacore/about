3276eef8a6a593ca23efb629db13a317fd7dba46
Viktor Yakubiv <viktor@yakubiv.com>
2019-01-16 18:02:54 +0200

    implement join-us section

--- components/sections/index.js
+++ components/sections/index.js
@@ -1,2 +1,2 @@
-// eslint-disable-next-line import/prefer-default-export
 export { default as DescriptionSection } from './description'
+export { default as JoinSection } from './join'
---
+++ components/sections/join.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Button, Container } from 'reactstrap'
+import { Content, Section } from '../content'
+import Link from '../link'
+
+import './join.scss'
+
+const JoinSection = ({
+  id,
+  title,
+  lead,
+  action,
+  note,
+  className = '',
+  ...restProps
+}) => (
+  <Section id={id} className={`join-section ${className}`} {...restProps}>
+    <Container>
+      <h2>{title}</h2>
+      <Content className="join-section-lead" markdown>
+        {lead}
+      </Content>
+      <Link href="~join" passHref>
+        <Button className="join-core-button" size="lg">
+          {action}
+        </Button>
+      </Link>
+      <footer className="join-section-note">
+        <Content markdown>{note}</Content>
+      </footer>
+    </Container>
+  </Section>
+)
+
+export default JoinSection
