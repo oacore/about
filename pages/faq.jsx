0346a764bdaaa4c6d95b1d419688071188cf86b1
Viktor Yakubiv <viktor@yakubiv.com>
2019-01-16 14:24:55 +0200

    add subsections to FAQs page

--- pages/faq.jsx
+++ pages/faq.jsx
@@ -9,20 +9,37 @@ const itemToURL = id => {
   window.history.replaceState({}, null, url.toString())
 }
 
+const FAQsSection = ({
+  id,
+  title,
+  items = [],
+  sections = [],
+  caption = title,
+  level = 2,
+}) => {
+  const Heading = `h${level}`
+  return (
+    <Section id={id} caption={caption}>
+      <Heading>{title}</Heading>
+      <Accordion onToggle={itemToURL}>
+        {items.map(({ slug, question, answer }) => (
+          <Accordion.Item id={slug} title={question} key={slug}>
+            <Content markdown>{answer}</Content>
+          </Accordion.Item>
+        ))}
+      </Accordion>
+      {sections.map(section => (
+        <FAQsSection key={section.title} level={level + 1} {...section} />
+      ))}
+    </Section>
+  )
+}
+
 const FAQsPage = () => (
   <Article container>
     <h1>{faqData.title}</h1>
     {faqData.sections.map(section => (
-      <Section caption={section.title} key={section.title}>
-        <h2>{section.title}</h2>
-        <Accordion onToggle={itemToURL}>
-          {section.items.map(({ slug, question, answer }) => (
-            <Accordion.Item id={slug} title={question} key={slug}>
-              <Content markdown>{answer}</Content>
-            </Accordion.Item>
-          ))}
-        </Accordion>
-      </Section>
+      <FAQsSection key={section.title} {...section} />
     ))}
   </Article>
 )
