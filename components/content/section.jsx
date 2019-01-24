f29185a73c647e1e33f205df7ab7db0f82c633f8
Viktor Yakubiv <viktor@yakubiv.com>
2019-01-24 14:31:32 +0200

    add section sizes and spacing

--- components/content/section.jsx
+++ components/content/section.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
-const Section = ({ children, className, tag: Tag = 'section', ...args }) => (
-  <Tag className={`section ${className || ''}`} {...args}>
-    {children}
-  </Tag>
-)
+const Section = ({
+  children,
+  large = false,
+  small = false,
+  className = '',
+  tag: Tag = 'section',
+  ...restProps
+}) => {
+  const classNames = [
+    'section',
+    large ? 'section-lg' : '',
+    small ? 'section-sm' : '',
+    className,
+  ].join(' ')
+  return (
+    <Tag className={classNames} {...restProps}>
+      {children}
+    </Tag>
+  )
+}
 
 export default Section
