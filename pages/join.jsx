248d95dc03c92039eae5e903e1509951cdf55ad9
Viktor Yakubiv <viktor@yakubiv.com>
2019-02-05 16:55:00 +0200

    add Join page placeholder

---
+++ pages/join.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Article } from 'components'
+import { title } from 'data/join.yml'
+
+const JoinPage = () => (
+  <Article tag="main">
+    <h1>{title}</h1>
+  </Article>
+)
+
+export default JoinPage
