244248133aa840138284d0e8f84c1c4b20521290
Serg M <js.develop.com@gmail.com>
2019-02-08 18:06:34 +0200

    restyling services page

--- pages/services/index.jsx
+++ pages/services/index.jsx
@@ -18,41 +18,57 @@ const ServicesPage = () => (
         tag="div"
       >
         <Row>
-          <Col xs="12" lg="9" tag="section">
-            <h2 id={sections.id} key={sections.id}>
+          <Col xs="12">
+            <h2 id={sections.id} key={sections.id} className="text-center">
               {sections.title}
             </h2>
 
-            {sections.sections.map(subsections => (
-              <div>
-                <Content
-                  className="h3 mt-4"
-                  markdown
-                  id={subsections.title}
-                  key={subsections.title}
-                >
-                  {subsections.title}
-                </Content>
+            <Row>
+              <Col xs="12" md="5">
+                <img
+                  className="services-logo d-block ml-md-auto  m-xs-auto my-1"
+                  src="/static/images/logos/jisc.svg"
+                  alt="Jisc"
+                />
+                <img
+                  className="services-screenshot d-block  ml-md-auto  m-xs-auto w-100"
+                  src="/static/images/map.png"
+                  alt="map"
+                />
+              </Col>
+              <Col xs="12" sm="7">
+                {sections.sections.map(subsections => (
+                  <div>
+                    <Content
+                      className="h3 mt-4"
+                      markdown
+                      id={subsections.title}
+                      key={subsections.title}
+                    >
+                      {subsections.title}
+                    </Content>
 
-                <Content
-                  markdown
-                  id={subsections.description}
-                  key={subsections.description}
-                >
-                  {subsections.description}
-                </Content>
+                    <Content
+                      markdown
+                      id={subsections.description}
+                      key={subsections.description}
+                    >
+                      {subsections.description}
+                    </Content>
 
-                <Link href={subsections.action.url} passHref>
-                  <Button
-                    outline
-                    id={subsections.action.caption}
-                    key={subsections.action.caption}
-                  >
-                    {subsections.action.caption}
-                  </Button>
-                </Link>
-              </div>
-            ))}
+                    <Link href={subsections.action.url} passHref>
+                      <Button
+                        outline
+                        id={subsections.action.caption}
+                        key={subsections.action.caption}
+                      >
+                        {subsections.action.caption}
+                      </Button>
+                    </Link>
+                  </div>
+                ))}
+              </Col>
+            </Row>
           </Col>
         </Row>
       </Section>
