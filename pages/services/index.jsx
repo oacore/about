37af5ebd6e58b1febf04ef88f9d3a57555431e6f
Serg M <js.develop.com@gmail.com>
2019-02-08 16:46:03 +0200

    change services page

--- pages/services/index.jsx
+++ pages/services/index.jsx
@@ -1,4 +1,63 @@
-import { SectionedPage } from '../../components/pages'
-import page from '../../data/services.yml'
+import React from 'react'
+import { Row, Col } from 'reactstrap'
+import { Article, Content, Section, Button } from 'components'
+import Link from 'components/link'
+import servicesData from 'data/services.yml'
 
-export default SectionedPage.create(page)
+import './services.scss'
+
+const ServicesPage = () => (
+  <Article nav tag="main">
+    <h1>{servicesData.title}</h1>
+
+    {servicesData.sections.map(sections => (
+      <Section
+        className="services-section"
+        caption={sections.title}
+        id={sections.id}
+        tag="div"
+      >
+        <Row>
+          <Col xs="12" lg="9" tag="section">
+            <h2 id={sections.id} key={sections.id}>
+              {sections.title}
+            </h2>
+
+            {sections.sections.map(subsections => (
+              <div>
+                <Content
+                  className="h3 mt-4"
+                  markdown
+                  id={subsections.title}
+                  key={subsections.title}
+                >
+                  {subsections.title}
+                </Content>
+
+                <Content
+                  markdown
+                  id={subsections.description}
+                  key={subsections.description}
+                >
+                  {subsections.description}
+                </Content>
+
+                <Link href={subsections.action.url} passHref>
+                  <Button
+                    outline
+                    id={subsections.action.caption}
+                    key={subsections.action.caption}
+                  >
+                    {subsections.action.caption}
+                  </Button>
+                </Link>
+              </div>
+            ))}
+          </Col>
+        </Row>
+      </Section>
+    ))}
+  </Article>
+)
+
+export default ServicesPage
