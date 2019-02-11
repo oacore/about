795aa093c296f1eeb756f99786916a61919401a4
Serg M <js.develop.com@gmail.com>
2019-02-11 15:14:02 +0200

    change page structure

--- pages/services/index.jsx
+++ pages/services/index.jsx
@@ -7,7 +7,7 @@ import servicesData from 'data/services.yml'
 import './services.scss'
 
 const ServicesPage = () => (
-  <Article nav tag="main">
+  <Article nav>
     <h1>{servicesData.title}</h1>
 
     {servicesData.sections.map(sections => (
@@ -23,31 +23,39 @@ const ServicesPage = () => (
               {sections.title}
             </h2>
 
-            <Row>
-              <Col xs="12" md="5">
-                <img
-                  className="services-logo d-block ml-md-auto  m-xs-auto my-1"
-                  src="/static/images/logos/jisc.svg"
-                  alt="Jisc"
-                />
-                <img
-                  className="services-screenshot d-block  ml-md-auto  m-xs-auto w-100"
-                  src="/static/images/map.png"
-                  alt="map"
-                />
-              </Col>
-              <Col xs="12" sm="7">
-                {sections.sections.map(subsections => (
-                  <div>
+            {sections.sections.map(subsections => (
+              <div>
+                <Row>
+                  <Col xs="6">
+                    <img
+                      className="services-logo"
+                      src={subsections.logo}
+                      alt="Logo"
+                    />
+                  </Col>
+
+                  <Col xs="6">
                     <Content
-                      className="h3 mt-4"
+                      className="h3 services-logo-text"
                       markdown
                       id={subsections.title}
                       key={subsections.title}
                     >
                       {subsections.title}
                     </Content>
+                  </Col>
+                </Row>
 
+                <Row>
+                  <Col md="5" lg="4">
+                    <img
+                      className="services-screenshot w-100"
+                      src={subsections.screenshot}
+                      alt=""
+                    />
+                  </Col>
+
+                  <Col md="7" lg="8">
                     <Content
                       markdown
                       id={subsections.description}
@@ -65,10 +73,10 @@ const ServicesPage = () => (
                         {subsections.action.caption}
                       </Button>
                     </Link>
-                  </div>
-                ))}
-              </Col>
-            </Row>
+                  </Col>
+                </Row>
+              </div>
+            ))}
           </Col>
         </Row>
       </Section>
