5a3f47bd9b9e92d679c099f5882eed6b2e626d2d
Viktor Yakubiv <viktor@yakubiv.com>
2019-02-13 12:45:50 +0200

    fix layout

--- pages/services/index.jsx
+++ pages/services/index.jsx
@@ -11,53 +11,42 @@ const ServicesPage = () => (
     <h1>{servicesData.title}</h1>
 
     {servicesData.sections.map(sections => (
-      <Section
-        className="services-section"
-        caption={sections.title}
-        id={sections.id}
-        tag="div"
-      >
-        <h2 id={sections.id} key={sections.id} className="text-center">
+      <Section id={sections.id} caption={sections.title}>
+        <h2 id={sections.id} className="text-center">
           {sections.title}
         </h2>
 
         {sections.sections.map(subsections => (
-          <div className="my-5">
-            <Row className="my-3">
-              {/* <Col xs="4" md="5" lg="4"> */}
-              <Col xs="4" sm="6">
+          <Section
+            key={subsections.id}
+            id={subsections.id}
+            className="service-section"
+          >
+            <Row tag="h2">
+              <Col sm="6" md="4" tag="span" className="service-section-logo">
                 <img
-                  className="services-logo"
                   src={subsections.logo}
-                  alt="Logo"
+                  alt={`${subsections.title}'s logo`}
                 />
               </Col>
 
-              {/* <Col xs="8"  md="7" lg="8"> */}
-              <Col xs="8" sm="6">
-                <Content
-                  className="h3 services-logo-text"
-                  markdown
-                  id={subsections.title}
-                  key={subsections.title}
-                >
-                  {subsections.title}
-                </Content>
+              <Col sm="6" md="8" tag="span">
+                {subsections.title}
               </Col>
             </Row>
 
-            <Row className="service-row-reverse">
-              {/* <Col md="5" lg="4"> */}
-              <Col sm="6">
-                <img
-                  className="services-screenshot w-100"
-                  src={subsections.screenshot}
-                  alt=""
-                />
+            <Row>
+              <Col sm="6" md="4">
+                <figure>
+                  <img
+                    className="service-section-screenshot"
+                    src={subsections.screenshot}
+                    alt={`${subsections.title}'s screenshot`}
+                  />
+                </figure>
               </Col>
 
-              {/* <Col md="7" lg="8"> */}
-              <Col sm="6">
+              <Col sm="6" md="8">
                 <div className="mb-5">
                   <Content
                     markdown
@@ -79,7 +68,7 @@ const ServicesPage = () => (
                 </div>
               </Col>
             </Row>
-          </div>
+          </Section>
         ))}
       </Section>
     ))}
