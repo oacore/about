f284f0ca70509a2c374caafbfc02aeb19e3e7b8a
Serg M <js.develop.com@gmail.com>
2019-02-21 12:45:03 +0200

    fix Free-package page names

--- pages/free-package.jsx
+++ pages/free-package.jsx
@@ -1,59 +1,51 @@
 import React from 'react'
 import { Row, Col } from 'reactstrap'
 import { Article, Content, Section, Button } from 'components/index'
-import servicesData from 'data/free-package-page.yml'
+import freeData from 'data/free-package-page.yml'
 
 import './free-package.scss'
 
 const ServicesPage = () => (
   <Article nav>
-    <h1>{servicesData.title}</h1>
+    <h1>{freeData.title}</h1>
 
-    {servicesData.sections.map(servicesGroup => (
-      <Section
-        key={servicesGroup.id}
-        id={servicesGroup.id}
-        caption={servicesGroup.title}
-      >
-        <h2 className="text-center">{servicesGroup.title}</h2>
+    {freeData.sections.map(freeGroup => (
+      <Section key={freeGroup.id} id={freeGroup.id} caption={freeGroup.title}>
+        <h2 className="text-center">{freeGroup.title}</h2>
 
-        {servicesGroup.sections.map(service => (
-          <Section key={service.id} id={service.id} className="service-section">
-            <Row className="service-section-title" tag="h3">
-              <Col sm="6" md="4" tag="span" className="service-section-logo">
-                <img src={service.logo} alt={`${service.title}'s logo`} />
+        {freeGroup.sections.map(free => (
+          <Section key={free.id} id={free.id} className="free-section">
+            <Row className="free-section-title" tag="h3">
+              <Col sm="6" md="4" tag="span" className="free-section-logo">
+                <img src={free.logo} alt={`${free.title}'s logo`} />
               </Col>
 
               <Col sm="6" md="8" tag="span">
-                {service.title}
+                {free.title}
               </Col>
             </Row>
 
-            <Row className="service-section-content">
+            <Row className="free-section-content">
               <Col sm="6" md="4">
                 <figure>
                   <img
-                    className="service-section-screenshot"
-                    src={service.screenshot}
-                    alt={`${service.title}'s screenshot`}
+                    className="free-section-screenshot"
+                    src={free.screenshot}
+                    alt={`${free.title}'s screenshot`}
                   />
                 </figure>
               </Col>
 
               <Col sm="6" md="8">
-                <Content
-                  markdown
-                  id={service.description}
-                  key={service.description}
-                >
-                  {service.description}
+                <Content markdown id={free.description} key={free.description}>
+                  {free.description}
                 </Content>
 
-                <footer className="service-section-footer">
-                  <Button color="primary" outline href={service.action.url}>
-                    {service.action.caption}
+                <footer className="free-section-footer">
+                  <Button color="primary" outline href={free.action.url}>
+                    {free.action.caption}
                   </Button>
-                  <Button color="primary" href={`~join?service=${service.id}`}>
+                  <Button color="primary" href={`~join?free=${free.id}`}>
                     Join CORE for free
                   </Button>
                 </footer>
