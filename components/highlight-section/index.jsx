f8ced146197091a972d67f67b9b7bf1a9ffeec3e
Serg M <js.develop.com@gmail.com>
2019-02-22 13:49:20 +0200

    add action to image at HighlightSection component

--- components/highlight-section/index.jsx
+++ components/highlight-section/index.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Row, Col } from 'reactstrap'
 import { Section } from '../content'
+import Link from '../link'
 
 import './highlight-section.scss'
 
-const HighlightSection = ({ image, children }) => (
+const HighlightSection = ({ image, action, children }) => (
   <Section className="highlight-section">
     <Row>
       <Col md="5" lg="4">
-        <img className="highlight-section-picture" src={image} alt="" />
+        <Link href={action} passHref>
+          <a href={action}>
+            <img className="highlight-section-picture" src={image} alt="" />
+          </a>
+        </Link>
       </Col>
       <Col md="7" lg="8">
         {children}
