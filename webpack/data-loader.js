5cab6011347cda0e91febbc623d56ce8e29f5dbb
Viktor Yakubiv <viktor@yakubiv.com>
2019-02-27 12:10:09 +0200

    patch data-files with statistics
    The all files are pathced by all the statistics. It's better to
    make template literals replacement right during the build process

--- webpack/data-loader.js
+++ webpack/data-loader.js
@@ -1,5 +1,37 @@
 const camelize = require('camelize')
+const fetch = require('node-fetch')
 
-const dataLoader = content => JSON.stringify(camelize(JSON.parse(content)))
+// Hack to avoid data retrieval in the all components
+// TODO: Implement Babel plugin instead
+const statsUrl = 'https://api.core.ac.uk/internal/statistics'
+
+const retrieveStats = url => {
+  let stats = null
+  return new Promise((resolve, reject) => {
+    if (stats == null) {
+      fetch(url)
+        .then(res => res.json())
+        .then(data => {
+          stats = data
+          resolve(stats)
+        })
+        .catch(reject)
+    } else resolve(stats)
+  })
+}
+
+const dataLoader = function loadDataFile(content) {
+  const callback = this.async()
+  const dataFile = camelize(JSON.parse(content))
+  retrieveStats(statsUrl).then(statistics =>
+    callback(
+      null,
+      JSON.stringify({
+        ...dataFile,
+        statistics,
+      })
+    )
+  )
+}
 
 module.exports = dataLoader
