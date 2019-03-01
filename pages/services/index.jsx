e8483df01dd31d28bce32e256fc9dbcffec21780
Serg M <js.develop.com@gmail.com>
2019-03-01 13:23:21 +0200

    Remove the Join CORE for free buttons from the overakll services page

--- pages/services/index.jsx
+++ pages/services/index.jsx
@@ -53,9 +53,6 @@ const ServicesPage = () => (
                   <Button color="primary" outline href={service.action.url}>
                     {service.action.caption}
                   </Button>
-                  <Button color="primary" href={`~join?service=${service.id}`}>
-                    Join CORE for free
-                  </Button>
                 </footer>
               </Col>
             </Row>
