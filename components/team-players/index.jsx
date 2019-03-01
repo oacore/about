396de81b24139928f4c40f6d877a528a052dcd5c
Serg M <js.develop.com@gmail.com>
2019-03-01 17:07:22 +0200

    add event handler to Team card

--- components/team-players/index.jsx
+++ components/team-players/index.jsx
@@ -11,56 +11,57 @@ import {
 
 import './team-players.scss'
 
-const TeamPlayers = ({
-  name,
-  role,
-  picture,
-  description,
-  className = '',
-  ...restProps
-}) => (
-  <div
-    className={`team-member image-flip  ${className}`}
-    {...restProps}
-    ontouchstart="this.classList.toggle('hover');"
-  >
-    <div className="mainflip">
-      <div className="frontside">
-        <Card>
-          <div className="team-card-image">
-            <CardImg src={picture} alt={name} />
+class TeamPlayers extends React.Component {
+  state = {
+    toggle: false,
+  }
+
+  handleToggle = () => {
+    this.setState({ toggle: true })
+  }
+
+  render() {
+    const { name, role, picture, description, ...restProps } = this.props
+    const { toggle } = this.state
+
+    return (
+      <div
+        id="team-players"
+        className={`team-member image-flip ${toggle ? 'image-flip-touch' : ''}`}
+        {...restProps}
+      >
+        <div className="mainflip">
+          <div className="frontside">
+            <Card>
+              <div className="team-card-image">
+                <CardImg src={picture} alt={name} />
+              </div>
+              <CardBody>
+                <CardTitle className="h4">{name}</CardTitle>
+                <CardSubtitle className="team-card-role">{role}</CardSubtitle>
+                <Button
+                  onClick={this.handleToggle}
+                  color="primary"
+                  className="my-3"
+                  outline
+                >
+                  Details
+                </Button>
+              </CardBody>
+            </Card>
           </div>
-          <CardBody>
-            <CardTitle className="h4">{name}</CardTitle>
-            <CardSubtitle className="team-card-role">{role}</CardSubtitle>
-          </CardBody>
-          <Button color="primary" outline className="py-2">
-            Details
-          </Button>
-        </Card>
+          <div className="backside">
+            <Card>
+              <CardBody className="mt-4">
+                <CardTitle className="h4">{name}</CardTitle>
+                <CardText>{description}</CardText>
+              </CardBody>
+            </Card>
+          </div>
+        </div>
       </div>
-      <div className="backside">
-        <Card>
-          <CardBody className="mt-4">
-            <CardTitle className="h4">{name}</CardTitle>
-            <CardText>{description}</CardText>
-            <ul className="list-inline text-center">
-              <li className="list-inline-item">
-                <a className="social-icon" target="_blank" href="/">
-                  <i className="fa fa-2x fa-facebook" />
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a className="social-icon" target="_blank" href="/">
-                  <i className="fa fa-2x fa-twitter" />
-                </a>
-              </li>
-            </ul>
-          </CardBody>
-        </Card>
-      </div>
-    </div>
-  </div>
-)
+    )
+  }
+}
 
 export default TeamPlayers
