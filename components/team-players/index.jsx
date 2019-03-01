6993f969be482a7225c67f3ae86475c2967f6cd7
Serg M <js.develop.com@gmail.com>
2019-03-01 11:07:57 +0200

    change card style

--- components/team-players/index.jsx
+++ components/team-players/index.jsx
@@ -26,38 +26,32 @@ const TeamPlayers = ({
   >
     <div className="mainflip">
       <div className="frontside">
-        <Card className="card">
+        <Card>
           <div className="team-card-image">
             <CardImg src={picture} alt={name} />
           </div>
-          <CardBody className="card-body text-center">
+          <CardBody>
             <CardTitle className="h4">{name}</CardTitle>
             <CardSubtitle className="team-card-role">{role}</CardSubtitle>
-            <Button color="primary">Details</Button>
           </CardBody>
+          <Button color="primary" outline className="py-3">
+            Details
+          </Button>
         </Card>
       </div>
       <div className="backside">
-        <Card className="card">
-          <CardBody className="card-body text-center mt-4">
+        <Card>
+          <CardBody className="mt-4">
             <CardTitle className="h4">{name}</CardTitle>
             <CardText>{description}</CardText>
-            <ul className="list-inline">
+            <ul className="list-inline text-center">
               <li className="list-inline-item">
-                <a
-                  className="social-icon text-xs-center"
-                  target="_blank"
-                  href="/"
-                >
+                <a className="social-icon" target="_blank" href="/">
                   <i className="fa fa-2x fa-facebook" />
                 </a>
               </li>
               <li className="list-inline-item">
-                <a
-                  className="social-icon text-xs-center"
-                  target="_blank"
-                  href="/"
-                >
+                <a className="social-icon" target="_blank" href="/">
                   <i className="fa fa-2x fa-twitter" />
                 </a>
               </li>
