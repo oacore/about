93d4a8535fa399d03181c601544a1d7bfce6066d
Serg M <js.develop.com@gmail.com>
2019-03-04 11:21:30 +0200

    add new icons to Whos using CORE section

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -60,7 +60,8 @@ const AboutPage = () => (
     </Section>
 
     <Section className="about-endorsements-section text-center">
-      <img src={aboutData.endorsements.picture} alt="icon" />
+      <img src={aboutData.endorsements.picture1} alt="icon" />
+      <img src={aboutData.endorsements.picture2} alt="icon" />
       <h2>{aboutData.endorsements.title}</h2>
       <Content markdown>{aboutData.endorsements.content}</Content>
       <Link href="~about/endorsements" passHref>
