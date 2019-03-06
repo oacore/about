8f21eca9be19f0d76f040a3b7ce0e2d9ed45b001
Viktor Yakubiv <viktor@yakubiv.com>
2019-03-06 17:56:34 +0200

    pass parameters to search-field

--- components/search/forms/simple.jsx
+++ components/search/forms/simple.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Button, InputGroup, InputGroupAddon, Input } from 'reactstrap'
 
-const SearchField = ({ placeholder, size = '' }) => (
+const SearchField = ({ size = '', ...fieldProps }) => (
   <InputGroup size={size}>
-    <Input placeholder={placeholder} />
+    <Input type="search" {...fieldProps} />
     <InputGroupAddon addonType="append">
       <Button color="primary">Search</Button>
     </InputGroupAddon>
