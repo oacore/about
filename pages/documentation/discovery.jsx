c8d2eed0a123580c02dd2d3c043c67dad4a2a682
Serg M <js.develop.com@gmail.com>
2019-03-11 15:07:00 +0200

    create separate services documentation pages

---
+++ pages/documentation/discovery.jsx
@@ -0,0 +1,4 @@
+import { MarkdownPage } from 'components/pages'
+import page from 'data/documentation/discovery.md'
+
+export default MarkdownPage.create(page)
---
+++ pages/documentation/repository-dashboard.jsx
@@ -0,0 +1,4 @@
+import { MarkdownPage } from 'components/pages'
+import page from 'data/documentation/repository-dashboard.md'
+
+export default MarkdownPage.create(page)
