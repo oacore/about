d70adbaa7f5e3ac8c5f404b42bc070ad0e3e733e
Serg M <js.develop.com@gmail.com>
2019-03-20 17:05:37 +0200

    develop ServiceGroups component

---
+++ components/service-groups/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Row, Col } from 'reactstrap'
+import ServiceGroupCard from '../service-group-card'
+
+const ServiceGroups = ({ items, className = '', ...restProps }) => (
+  <Row className={`service-groups ${className}`} tag="ul" {...restProps}>
+    {items.map(group => (
+      <Col key={group.id} sm="6" lg="4" tag="li">
+        <ServiceGroupCard title={group.title} items={group.sections} />
+      </Col>
+    ))}
+  </Row>
+)
+
+export default ServiceGroups
