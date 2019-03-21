c3a1afd542401d182ff2c5ff1771c8b1eb35f02f
Viktor Yakubiv <viktor@yakubiv.com>
2019-03-21 18:13:24 +0200

    add label for main search field

--- components/search/forms/simple.jsx
+++ components/search/forms/simple.jsx
@@ -1,18 +1,37 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Button, InputGroup, InputGroupAddon, Input } from 'reactstrap'
 
-const SearchField = ({ size = '', ...fieldProps }) => (
-  <InputGroup size={size}>
-    <Input type="search" {...fieldProps} />
-    <InputGroupAddon addonType="append">
-      <Button color="primary">Search</Button>
-    </InputGroupAddon>
-  </InputGroup>
+// Stupid rule does not understand it
+/* eslint-disable jsx-a11y/label-has-for */
+
+const SearchField = ({
+  size = '',
+  id = 'search-form-field',
+  label = 'Search in CORE',
+  ...fieldProps
+}) => (
+  <Fragment>
+    <label className="sr-only" htmlFor={id}>
+      {label}
+    </label>
+    <InputGroup size={size}>
+      <Input type="search" id={id} {...fieldProps} />
+      <InputGroupAddon addonType="append">
+        <Button color="primary">Search</Button>
+      </InputGroupAddon>
+    </InputGroup>
+  </Fragment>
 )
 
-const SearchForm = ({ action, method, onSubmit, ...fieldProps }) => (
-  <form action={action} method={method} onSubmit={onSubmit}>
-    <SearchField size="lg" {...fieldProps} />
+const SearchForm = ({
+  action,
+  method,
+  onSubmit,
+  id = 'search-form',
+  ...fieldProps
+}) => (
+  <form id={id} action={action} method={method} onSubmit={onSubmit}>
+    <SearchField id={`${id}-field`} size="lg" {...fieldProps} />
   </form>
 )
 
