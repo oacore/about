e8b3687b5b2b636f9b3bff6058b0ad5ce076899a
Viktor Yakubiv <viktor@yakubiv.com>
2019-03-21 13:23:07 +0200

    improve components general exports

--- components/index.js
+++ components/index.js
@@ -1,16 +1,46 @@
 // Central Bootstrap style usage
 import './bootstrap/bootstrap.scss'
 
-// TODO: Order components correctly (discuss 'correctly')
-export { default as Header } from './header'
-export { default as Footer } from './footer'
-export { default as Layout } from './layout'
-export { default as Hero } from './hero'
-export { default as Accordion } from './accordion'
-export { default as Logo } from './logo'
-export { KeyFeature, KeyFeatureList } from './key-feature'
-export { default as Switcher, SwitcherItem } from './switcher'
-export { default as Collapsed } from './collapsed'
-export * from './content'
+// Base
 export * from './elements'
 export { default as Markdown } from './markdown'
+
+// Layout
+export * from './content'
+export { default as Layout } from './layout'
+
+// More complex layout
+export { default as Header } from './header'
+export { default as SearchNavbar } from './search-navbar'
+export { default as Footer } from './footer'
+
+// Elements
+export { default as Logo } from './logo'
+export { default as Blockquote } from './blockquote'
+export { KeyFeature, KeyFeatureList } from './key-feature'
+export { default as Testimonial } from './testimonial'
+export { default as TeamMember } from './team-member'
+export { default as Pagination } from './pagination'
+export { default as Search } from './search'
+
+// Complex elements
+export { default as Accordion } from './accordion'
+export { default as Collapsed } from './collapsed'
+
+// Forms
+export * from './cookies'
+
+// Dynamic (carousels, switchers)
+export { default as Switcher, SwitcherItem } from './switcher'
+export { default as LogosCarousel } from './logos-carousel'
+export { default as TestimonialsCarousel } from './testimonials-carousel'
+
+// Dynamic data fetching
+export { default as RepositoriesBrowser } from './repositories-browser'
+export { default as RepositoriesMap } from './repositories-map'
+export { default as RepositoriesSearch } from './repositories-search'
+export { default as Blog } from './blog'
+
+// Sections
+export { default as Hero } from './hero'
+export { default as HighlightSection } from './highlight-section'
--- components/sections/index.js
+++ components/sections/index.js
@@ -1,2 +1,3 @@
 export { default as DescriptionSection } from './description'
+export { default as EndorsementsSection } from './endorsements'
 export { default as JoinSection } from './join'
