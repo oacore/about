7017856cf0cce8bbfdb1120b0a01cd89665aac55
Viktor Yakubiv <viktor@yakubiv.com>
2019-03-26 15:47:55 +0200

    remove redundant free-package data

--- pages/services/fast-sync.jsx
+++ pages/services/fast-sync.jsx
@@ -1,5 +1,4 @@
 import { ServicePage } from 'components/pages'
 import page from 'data/services/fast-sync.yml'
-import freePackage from 'data/services/free-package.yml'
 
-export default ServicePage.create(page, freePackage)
+export default ServicePage.create(page)
--- pages/services/recommender.jsx
+++ pages/services/recommender.jsx
@@ -1,5 +1,4 @@
 import { ServicePage } from 'components/pages'
 import page from 'data/services/recommender.yml'
-import freePackage from 'data/services/free-package.yml'
 
-export default ServicePage.create(page, freePackage)
+export default ServicePage.create(page)
