2ac29ded7ae722ec9d24f1ab9f8b2845ec788d9c
Viktor Yakubiv <viktor@yakubiv.com>
2019-03-26 16:56:13 +0200

    add favicon

--- components/layout/head.jsx
+++ components/layout/head.jsx
@@ -7,6 +7,7 @@ const Head = ({ title, description, keywords, children, ...restProps }) => (
       name="viewport"
       content="width=device-width, initial-scale=1, shrink-to-fit=no"
     />
+
     <title>{title}</title>
     {description && <meta name="description" content={description} />}
     {keywords && (
@@ -15,6 +16,11 @@ const Head = ({ title, description, keywords, children, ...restProps }) => (
         content={Array.isArray(keywords) ? keywords.join(', ') : keywords}
       />
     )}
+
+    <link rel="shortcut icon" href="/static/favicon/favicon-32px.png" />
+    <link rel="icon" href="/static/favicon/favicon-128px.png" />
+    <link rel="icon" href="/static/favicon/favicon.svg" />
+
     {children}
   </NextHead>
 )
