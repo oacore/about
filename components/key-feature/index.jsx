a6a9b986bb5696dcb21d23033ea5532d18cf8a05
Viktor Yakubiv <viktor@yakubiv.com>
2019-03-27 15:00:55 +0200

    support linkable key-features with href passed

--- components/key-feature/index.jsx
+++ components/key-feature/index.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { Link } from '../elements'
 
 import './key-feature.scss'
 
@@ -6,22 +7,43 @@ const KeyFeature = ({
   title,
   children,
   icon,
+  href,
   className = '',
-  tag: Tag = 'div',
-  ...args
-} = {}) => (
-  <Tag className={`key-feature ${className || ''}`} {...args}>
-    <img className="key-feature-icon" src={icon} alt={title} />
-    <div className="key-feature-text">{children}</div>
-  </Tag>
-)
+  tag: Tag,
+  ...restProps
+} = {}) => {
+  const WrapperTag = Tag || href ? Fragment : 'div'
+  const ContentTag = Tag !== 'a' && href ? 'a' : Fragment
+
+  const contentProps = {
+    className: 'key-feature-link',
+  }
+
+  if (WrapperTag === Fragment) {
+    Object.assign(
+      contentProps,
+      { className: `key-feature ${className} ${contentProps.className}` },
+      restProps
+    )
+  }
+
+  return (
+    <WrapperTag className={`key-feature ${className}`} {...restProps}>
+      <Link href={href} passHref>
+        <ContentTag {...contentProps}>
+          <img className="key-feature-icon" src={icon} alt={title} />
+          <div className="key-feature-text">{children}</div>
+        </ContentTag>
+      </Link>
+    </WrapperTag>
+  )
+}
 
 const KeyFeatureList = ({ children, className = '' } = {}) => {
   const items = React.Children.map(children, child => {
     if (child.type !== KeyFeature) return null
 
     return React.cloneElement(child, {
-      tag: 'li',
       key: child.props.title,
     })
   })
