3c995f24cdafc8fc54c22e165fca7b141dd2bd4d
Viktor Yakubiv <viktor@yakubiv.com>
2019-04-01 15:33:55 +0300

    fix markdown links rendering

--- components/markdown.jsx
+++ components/markdown.jsx
@@ -6,14 +6,11 @@ import Link from './link'
 
 // We pass additional link because Markdown component does not render
 // pure text but renders own TextRenderer componet instead
-const MarkdownLink = ({ href, title, children }) => {
-  const [pathname, hash] = href.split('#')
-  return (
-    <Link href={{ pathname, hash: hash && `#${hash}` }} passHref>
-      <a title={title}>{children}</a>
-    </Link>
-  )
-}
+const MarkdownLink = ({ href, title, children }) => (
+  <Link href={href} passHref>
+    <a title={title}>{children}</a>
+  </Link>
+)
 
 const MarkdownImage = ({ src, alt, className = '', ...restProps }) => (
   <img
