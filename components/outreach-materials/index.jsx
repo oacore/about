d83d458972630b9b8384dd26bfed4353f8bcbc2c
Serg M <js.develop.com@gmail.com>
2019-08-09 17:05:05 +0300

    Add OutreachMaterials component
    The components displays resources as cards

---
+++ components/outreach-materials/index.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import {
+  Card,
+  CardImg,
+  CardTitle,
+  CardText,
+  CardSubtitle,
+  CardBody,
+  CardLink,
+} from 'reactstrap'
+
+import './outreach-materials.scss'
+
+const OutreachMaterials = ({
+  className = '',
+  name,
+  role,
+  picture,
+  country,
+  description,
+  button,
+  link,
+  ...restProps
+}) => (
+  <Card className={`outreach-materials ${className}`} {...restProps}>
+    <div className="outreach-materials-picture">
+      <CardImg src={picture} alt={`${name}'s photo`} />
+    </div>
+    <CardBody>
+      <CardTitle className="p outreach-materials-name">{name}</CardTitle>
+      <CardSubtitle className="outreach-materials-role">{role}</CardSubtitle>
+      <CardText>{country}</CardText>
+      <CardText>{description}</CardText>
+      <CardLink className="btn btn-outline-primary w-100" href={link}>
+        {button}
+      </CardLink>
+    </CardBody>
+  </Card>
+)
+
+export default OutreachMaterials
