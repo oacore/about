c854dd0ecd428f4987acec96a7dc589339bf2e4d
Serg M <js.develop.com@gmail.com>
2019-08-09 17:05:05 +0300

    Add OutreachMaterials component
    The components displays resources as cards
    
    Remove shadow from OutreachMaterials

---
+++ components/outreach-materials/index.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import {
+  Card,
+  CardImg,
+  CardTitle,
+  CardText,
+  CardSubtitle,
+  CardBody,
+  CardLink,
+} from 'reactstrap'
+
+import './outreach-materials.scss'
+
+const format2name = type =>
+  ({
+    jpg: 'JPEG',
+    jpeg: 'JPEG',
+    png: 'PNG',
+    pdf: 'PDF',
+    gdoc: 'Google Doc',
+    gslides: 'Google Slides',
+  }[type])
+
+const OutreachMaterials = ({
+  id,
+  className = '',
+  name,
+  role,
+  picture,
+  country,
+  description,
+  format,
+  link,
+  ...restProps
+}) => (
+  <Card id={id} className={`outreach-materials ${className}`} {...restProps}>
+    <div className="outreach-materials-picture">
+      {picture && <CardImg src={picture} alt={`${name}'s image`} />}
+    </div>
+
+    <CardBody>
+      <CardTitle className="p outreach-materials-name">{name}</CardTitle>
+      <CardSubtitle className="outreach-materials-role">{role}</CardSubtitle>
+      <CardText>{country}</CardText>
+      <CardText>{description}</CardText>
+
+      {['gdoc', 'gslides'].includes(format) ? (
+        <CardLink
+          className="btn btn-outline-primary w-100"
+          href={link}
+          target="_blank"
+          rel="noopener"
+        >
+          Open {format2name(format)}
+        </CardLink>
+      ) : (
+        <CardLink
+          className="btn btn-outline-primary w-100"
+          href={link}
+          download={`core-${id}.${format}`}
+        >
+          Download {format2name(format)}
+        </CardLink>
+      )}
+    </CardBody>
+  </Card>
+)
+
+export default OutreachMaterials
