bf01ebb656b91ec5b38feea00917f7d4599b9ef5
Serg M <js.develop.com@gmail.com>
2019-08-29 15:37:47 +0300

    Add ambassadors coordinates

--- components/world-map-pin/index.jsx
+++ components/world-map-pin/index.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 import './world-map-pin.scss'
 
 const Pin = ({ latitude, longitude, picture }) => {
-  const x = 48.8 + (parseFloat(longitude) / 360) * 100
-  const y = 56 + (parseFloat(latitude) / 180) * -100
+  const x = 50 + (parseFloat(longitude) / 360) * 100
+  const y = 50 + (parseFloat(latitude) / 180) * -100
+  // const x = 48.8 + (parseFloat(longitude) / 360) * 100
+  // const y = 56 + (parseFloat(latitude) / 180) * -100
 
   return (
     <div className="pin" style={{ top: `${y}%`, left: `${x}%` }}>
