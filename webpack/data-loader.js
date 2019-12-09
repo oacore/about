fa5cd2bd240d8e4551dfa1154d305fb5fa59a834
Serg M <js.develop.com@gmail.com>
2019-12-09 13:02:58 +0200

    Add statistics caching
    Adds caching mechanism for statistics for local development
    without internet connection or for the cases when API is down.

--- webpack/data-loader.js
+++ webpack/data-loader.js
@@ -1,11 +1,21 @@
+const { promises: fsPromises } = require('fs')
+const path = require('path')
 const camelize = require('camelize')
 const fetch = require('node-fetch')
 
 // Hack to avoid data retrieval in the all components
 // TODO: Implement Babel plugin instead
 const statsUrl = 'https://api.core.ac.uk/internal/statistics'
+const cachePath =
+  process.env.NODE_ENV === 'production'
+    ? ''
+    : path.join(__dirname, 'statistics.json.cache')
+const defaultStatsPath =
+  process.env.NODE_ENV === 'production'
+    ? ''
+    : path.join(__dirname, 'statistics.json')
 
-const retrieveStats = url => {
+const fetchStats = url => {
   let stats = null
   return new Promise((resolve, reject) => {
     if (stats == null) {
@@ -20,10 +30,42 @@ const retrieveStats = url => {
   })
 }
 
+const saveCachedStats = (cacheFilePath, statisticsData) => {
+  return fsPromises.writeFile(cacheFilePath, statisticsData)
+}
+
+const loadCachedStats = async (cacheFilePath, { ignoreModified = false }) => {
+  if (!cacheFilePath) throw new Error('Cache path cannot be empty')
+
+  if (!ignoreModified) {
+    const cacheStats = await fsPromises.stat(cacheFilePath)
+    if (cacheStats.mtimeMs < Date.now() - 6 * 60 * 60 * 1000)
+      throw new Error('Cache outdated')
+  }
+
+  return fsPromises
+    .readFile(cacheFilePath)
+    .then(contents => JSON.parse(contents))
+}
+
+const retrieveStats = async (url, catchFilePath) => {
+  try {
+    return await loadCachedStats(catchFilePath)
+  } catch (cacheError) {
+    try {
+      const stats = await fetchStats(url)
+      saveCachedStats(catchFilePath, JSON.stringify(stats))
+      return stats
+    } catch (fetchError) {
+      return loadCachedStats(defaultStatsPath, { ignoreModified: true })
+    }
+  }
+}
+
 const dataLoader = function loadDataFile(content) {
   const callback = this.async()
   const dataFile = camelize(JSON.parse(content))
-  retrieveStats(statsUrl).then(statistics =>
+  retrieveStats(statsUrl, cachePath).then(statistics =>
     callback(
       null,
       JSON.stringify({
