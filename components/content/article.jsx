ebf0cf6f538dd94fedddf2125477ceac53849754
Jozef Harag <32jojo32@gmail.com>
2020-08-25 15:33:20 +0200

    Refactor sections

--- components/content/article.jsx
+++ components/content/article.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
-import { Nav, NavItem, NavLink } from 'reactstrap'
-
-import Section from './section'
+import {
+  Container as BootstrapContainer,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap'
 
 const ArticleNav = ({ items }) => (
   <Nav className="article-nav">
@@ -14,84 +17,64 @@ const ArticleNav = ({ items }) => (
   </Nav>
 )
 
-class Article extends Section {
-  state = {
-    header: null,
-    content: null,
-    navItems: [],
-  }
+const Article = ({ children, ...props }) => {
+  const [content, setContent] = useState([])
+  const [header, setHeader] = useState([])
+  const [navItems, setNavItems] = useState([])
 
-  static getDerivedStateFromProps({ children }, state) {
-    const navItems = React.Children.map(children, child => {
-      if (child == null || !Section.isSection(child)) return null
+  useEffect(() => {
+    const headerInner = []
+    const contentInner = []
+    React.Children.forEach(children, child => {
+      if (!child) return
+      if (typeof child.type == 'string' && child.type.match(/header|h[1-6]/))
+        headerInner.push(child)
+      else contentInner.push(child)
+    })
+
+    const navItemsInner = React.Children.map(children, child => {
+      if (child == null) return null
       const { caption, id } = child.props
       if (!caption || !id) return null
       return { text: caption, href: `#${id}` }
     })
 
-    const header = []
-    const content = []
-    React.Children.forEach(children, child => {
-      if (!child) return
-      if (typeof child.type == 'string' && child.type.match(/header|h[1-6]/))
-        header.push(child)
-      else content.push(React.cloneElement(child, { caption: undefined }))
-    })
+    setNavItems(navItemsInner)
+    setHeader(headerInner)
+    setContent(contentInner)
+  }, [children])
 
-    return {
-      ...state,
-      header,
-      content,
-      navItems,
-    }
-  }
+  const renderHeader = () => {
+    const { nav } = props
 
-  renderHeader() {
-    const { nav, container } = this.props
-    const { header, navItems } = this.state
-
-    if (header.length === 0 && (!nav || navItems.length === 0)) return null
-
-    const Container = Section.getContainerComponent(container)
+    if (header.length === 0) return null
 
     return (
-      <Container>
+      <BootstrapContainer>
         {header}
         {nav && <ArticleNav items={navItems} />}
-      </Container>
+      </BootstrapContainer>
     )
   }
 
-  renderContent() {
-    const { container } = this.props
-    const { content } = this.state
-    const Container = Section.getContainerComponent(container)
+  const { nav, className, tag: Tag, ...restProps } = props
 
-    return Section.containerize(content, Container)
-  }
-
-  render() {
-    const { nav, className, tag: Tag, container, ...restProps } = this.props
-
-    return (
-      <Tag className={`article ${className || ''}`} {...restProps}>
-        {this.renderHeader()}
-        {this.renderContent()}
-      </Tag>
-    )
-  }
+  return (
+    <Tag className={`article ${className || ''}`.trim()} {...restProps}>
+      {renderHeader()}
+      {content}
+    </Tag>
+  )
 }
 
 Article.propTypes = {
   tag: PropTypes.node,
   nav: PropTypes.bool,
-  container: PropTypes.bool,
 }
 
 Article.defaultProps = {
   tag: 'article',
   nav: false,
-  container: true,
 }
 
 export default Article
--- components/error.jsx
+++ components/error.jsx
@@ -2,24 +2,24 @@ import React from 'react'
 
 import Page from './page'
 import ButtonToolbar from './button-toolbar'
-import { Content } from './content'
+import { Section } from './content'
 import { Button } from './elements'
 
 const ErrorPage = () => (
   <Page title="Page is not found">
     <h1>Uh-oh</h1>
-    <Content>
+    <Section tag="div">
       <p>The page you were looking for could not be found.</p>
       <p>To help you find what you are looking for, why not</p>
-    </Content>
-    <ButtonToolbar>
-      <Button color="primary" href="~home">
-        Go back to the homepage
-      </Button>
-      <Button color="primary" outline href="~contact">
-        Contact us
-      </Button>
-    </ButtonToolbar>
+      <ButtonToolbar>
+        <Button color="primary" href="~home">
+          Go back to the homepage
+        </Button>
+        <Button color="primary" outline href="~contact">
+          Contact us
+        </Button>
+      </ButtonToolbar>
+    </Section>
   </Page>
 )
 
