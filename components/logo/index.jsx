7cbec9b28230c4f4919e1148ac86249b2d48e91b
Jozef Harag <32jojo32@gmail.com>
2020-08-26 11:34:25 +0200

    Fix ref cannot be passed to functional component

--- components/logo/index.jsx
+++ components/logo/index.jsx
@@ -2,17 +2,21 @@ import React from 'react'
 
 import logoPath from './core-symbol.svg'
 
-const Logo = ({
-  text = 'CORE',
-  alt = text ? '' : 'CORE',
-  className = '',
-  tag: Tag = 'div',
-  ...restProps
-}) => (
+const Logo = React.forwardRef((
+  {
+    text = 'CORE',
+    alt = text ? '' : 'CORE',
+    className = '',
+    tag: Tag = 'div',
+    ...restProps
+  },
+  // eslint-disable-next-line no-unused-vars
+  _
+) => (
   <Tag className={`logo ${className}`} {...restProps}>
     <img src={logoPath} alt={alt} />
     {text}
   </Tag>
-)
+))
 
 export default Logo
