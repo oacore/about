9e66bb0336b398a89035356cd09142b9aa64fa8f
Jozef Harag <32jojo32@gmail.com>
2020-09-21 17:24:25 +0200

    Fix applying polyfills (#277)
    Caused hard refresh on any change

--- next.config.js
+++ next.config.js
@@ -7,7 +7,9 @@ const nextConfig = {
     const originalEntry = config.entry
     config.entry = async () => {
       const entries = await originalEntry()
-      if (entries['main.js']) entries['main.js'].unshift('./polyfills')
+
+      if (entries['main.js'] && !entries['main.js'].includes('./polyfills.js'))
+        entries['main.js'].unshift('./polyfills.js')
 
       return entries
     }
