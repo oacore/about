c61c163125845d0bd00ed6e2a5236f6898f10f4c
Jozef Harag <32jojo32@gmail.com>
2020-11-23 09:13:34 +0100

    Move leaflet css to map component

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,12 +2,6 @@ import React from 'react'
 
 import CookiesProvider from '../components/cookies/cookies-provider'
 
-// TODO: Move to map component once this is released
-//       https://github.com/vercel/next.js/issues/12079#issuecomment-678858809
-import 'leaflet/dist/leaflet.css'
-import 'leaflet.markercluster/dist/MarkerCluster.css'
-import 'leaflet.markercluster/dist/MarkerCluster.Default.css'
-
 import 'components/index.scss'
 
 import Main from 'main'
