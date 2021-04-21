c00d743b547a8f068854467d64b9e578b09b2949
vv2529 <vv02529@gmail.com>
2021-04-21 17:07:53 +0300

    Improve code and CMS config

--- pages/history.jsx
+++ pages/history.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const LinkCard = ({ link }) => (
   <div>
-    <h4>{link.caption}</h4>
+    <h4>
+      <a href={link.href} target="_blank" rel="noreferrer">
+        {link.caption}
+      </a>
+    </h4>
     <p>{link.source}</p>
-    <a href={link.href} target="_blank" rel="noreferrer"></a>
   </div>
 )
 
@@ -14,7 +17,7 @@ const HistoryEvent = ({ event }) => (
       <img src={event.image.src} alt={event.image.alt} />
     </div>
     <div>
-      <time dateTime={event.fullDate}>{event.date}</time>
+      <time dateTime={event.id}>{event.date}</time>
       <h3>{event.title}</h3>
       <p>{event.body}</p>
       <LinkCard link={event.link} />
@@ -25,8 +28,8 @@ const HistoryEvent = ({ event }) => (
 const HistoryPage = ({ data }) => (
   <>
     <p>{data?.body}</p>
-    {data.events.slice(1).map((event, index) => (
-      <HistoryEvent key={index} event={event} />
+    {data.events.slice(1).map((event) => (
+      <HistoryEvent key={event.fullDate} event={event} />
     ))}
     <p>{data.events[0]?.body}</p>
   </>
@@ -41,27 +44,11 @@ const getPageData = async () => {
 const getEvents = async () => {
   const events = []
 
-  const formatDate = (date) => {
-    // Assuming format: 'YYYY.MM.DD'
-    const getMonth = (dateString) => +dateString.slice(5, 7)
-
-    const monthNames = [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July',
-      'August',
-      'September',
-      'October',
-      'November',
-      'December',
-    ]
-
-    return `${monthNames[getMonth(date)]} ${date.slice(0, 4)}`
-  }
+  const formatDate = (date) =>
+    new Intl.DateTimeFormat('en-GB', {
+      year: 'numeric',
+      month: 'long',
+    }).format(Date.parse(date))
 
   const processedEvents = events.map((event) => ({
     ...event,
