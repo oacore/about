33d97f2c70471f05b32881b7fe57c55ef1e8253e
Viktor Yakubiv <viktor@yakubiv.com>
2021-04-26 20:30:40 +0300

    Migrate pure Markdown pages to the CMS (#392)
    1.  Removes every page that is basic `MarkdownPage.create()` in favour
        of the generic `[slug].jsx` that resolves the URL and finds
        the data file itself.
    
        `[slug].jsx` is a copy of `components/pages/markdown.jsx` instead
        the usage since the generic component might be redundant.
    
        There is documentation though 🤔
    
    2.  Cleans up data `*.md` files as well as repeated `*.jsx` files.
    
    3.  Adds basic pages connection to the Netlify CMS config.

--- pages/acknowledge.jsx
+++
@@ -1,4 +0,0 @@
-import { MarkdownPage } from 'components/pages'
-import page from 'data/dataset-acknowledge.md'
-
-export default MarkdownPage.create(page)
--- pages/ref-audit.jsx
+++
@@ -1,4 +0,0 @@
-import { MarkdownPage } from 'components/pages'
-import page from 'data/ref-audit.md'
-
-export default MarkdownPage.create(page)
