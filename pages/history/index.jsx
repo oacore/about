82d6fcb766e7b742ff32946ddbc73c6e09284c7b
vv2529 <vv02529@gmail.com>
2021-05-12 18:22:03 +0300

    Break down the logo and fix overflow in sections

--- pages/history/index.jsx
+++ pages/history/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
+import hatSrc from './assets/hat.svg'
 import styles from './history.module.scss'
 
-import { Markdown, Page } from 'components'
+import { Markdown, Page, Logo } from 'components'
 import historyData from 'data/history.yml'
 import retrieveContent from 'content'
+
+const parseDataAttributes = (data = {}) =>
+  Object.fromEntries(
+    Object.entries(data).map(([key, value]) => [`data-${key}`, value])
+  )
 
 const LinkCard = ({ link }) => (
   <div className={styles.card}>
@@ -17,10 +23,26 @@ const LinkCard = ({ link }) => (
   </div>
 )
 
-const Milestone = ({ milestone }) => (
-  <section className={styles.milestone}>
+const Milestone = ({
+  milestone,
+  data,
+  tag: Tag = 'div',
+  className = '',
+  ...htmlProps
+}) => (
+  <Tag
+    className={`${styles.milestone} ${className}`}
+    {...htmlProps}
+    {...parseDataAttributes(data)}
+  >
     <div className={styles['milestone-image-container']}>
-      <img src={milestone.image.src} alt={milestone.image.alt} />
+      {milestone.image && (
+        <img
+          src={milestone.image.src}
+          alt={milestone.image.alt}
+          className={styles['milestone-image']}
+        />
+      )}
     </div>
     <div className={styles['milestone-text-content']}>
       <time className={styles.date} dateTime={milestone.date.numeric}>
@@ -28,31 +50,62 @@ const Milestone = ({ milestone }) => (
       </time>
       <h3 className={styles['milestone-title']}>{milestone.title}</h3>
       <Markdown>{milestone.body}</Markdown>
-      <LinkCard link={milestone.link} />
+      {milestone.link && <LinkCard link={milestone.link} />}
     </div>
-  </section>
+  </Tag>
 )
 
+const RootMilestone = ({ children, className = '', ...htmlProps }) => (
+  <Markdown
+    className={`${styles['root-milestone']} ${className}`}
+    {...htmlProps}
+  >
+    {children}
+  </Markdown>
+)
+
+const Hero = ({ data, tag: Tag = 'div', className = '', ...htmlProps }) => (
+  <Tag className={`${styles.hero} ${className}}`} {...htmlProps}>
+    <RootMilestone className={styles.top}>{data?.body}</RootMilestone>
+  </Tag>
+)
+
+const AnniversaryLogo = () => (
+  <div className={styles['anniversary-logo']}>
+    <div className={styles['logo-circles']}>
+      <div className={`${styles['logo-circle']} ${styles['logo-circle-1']}`} />
+      <div className={`${styles['logo-circle']} ${styles['logo-circle-2']}`} />
+      <div className={`${styles['logo-circle']} ${styles['logo-circle-3']}`} />
+    </div>
+    <div className={styles['logo-content']}>
+      <Logo />
+      <div className={styles['logo-side-text']}>
+        <div className={styles['logo-1st-line']}>10</div>
+        <div className={styles['logo-2nd-line']}>years</div>
+      </div>
+    </div>
+    <img src={hatSrc} alt="" className={styles['logo-hat']} />
+  </div>
+)
+
+// TODO: Fix `id` attribute not being rendered to html
 const HistoryPage = ({ data }) => (
   <Page
     title={data.title}
     description={data.description}
     className={styles.article}
   >
-    <section className={styles['front-section']}>
-      <Markdown className={`${styles['root-milestone']} ${styles.top}`}>
-        {data?.body}
-      </Markdown>
-    </section>
-    {data.milestones.slice(1).map((milestone) => (
-      <Milestone key={milestone.id} milestone={milestone} />
-    ))}
-    <section
-      className={`${styles['root-milestone']} ${styles.bottom}`}
-      id="root"
-    >
-      <Markdown>{data.milestones[0]?.body}</Markdown>
-    </section>
+    <AnniversaryLogo />
+    <Hero data={data} />
+    {data.milestones
+      .slice(1)
+      .reverse()
+      .map((milestone) => (
+        <Milestone key={milestone.id} milestone={milestone} />
+      ))}
+    <RootMilestone className={styles.bottom} id="root">
+      {data.milestones[0]?.body}
+    </RootMilestone>
   </Page>
 )
 
