fa66328b9a7f225deb5fd8de5d3360a47aa278dd
vv2529 <vv02529@gmail.com>
2021-05-12 13:12:09 +0300

    Revert "Redesign components, backgrounds and more"
    This reverts commit e6ec858d51395cdb1d10cc859daff04b09dd5db0.

--- pages/history/index.jsx
+++ pages/history/index.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
 
-import logo from './assets/anniversary-logo-big.svg'
 import styles from './history.module.scss'
 
 import { Markdown, Page } from 'components'
 import historyData from 'data/history.yml'
 import retrieveContent from 'content'
-
-// console.log(logo)
-
-const parseDataAttributes = (data = {}) =>
-  Object.fromEntries(
-    Object.entries(data).map(([key, value]) => [`data-${key}`, value])
-  )
 
 const LinkCard = ({ link }) => (
   <div className={styles.card}>
@@ -25,22 +17,10 @@ const LinkCard = ({ link }) => (
   </div>
 )
 
-const Milestone = ({
-  milestone,
-  data,
-  tag: Tag = 'div',
-  className = '',
-  ...htmlProps
-}) => (
-  <Tag
-    className={`${styles.milestone} ${className}`}
-    {...htmlProps}
-    {...parseDataAttributes(data)}
-  >
+const Milestone = ({ milestone }) => (
+  <section className={styles.milestone}>
     <div className={styles['milestone-image-container']}>
-      {milestone.image && (
-        <img src={milestone.image.src} alt={milestone.image.alt} />
-      )}
+      <img src={milestone.image.src} alt={milestone.image.alt} />
     </div>
     <div className={styles['milestone-text-content']}>
       <time className={styles.date} dateTime={milestone.date.numeric}>
@@ -48,46 +28,31 @@ const Milestone = ({
       </time>
       <h3 className={styles['milestone-title']}>{milestone.title}</h3>
       <Markdown>{milestone.body}</Markdown>
-      {'link' in milestone ? <LinkCard link={milestone.link} /> : ''}
+      <LinkCard link={milestone.link} />
     </div>
-  </Tag>
+  </section>
 )
 
-const RootMilestone = ({ children, className = '', ...htmlProps }) => (
-  <Markdown
-    className={`${styles['root-milestone']} ${className}`}
-    {...htmlProps}
-  >
-    {children}
-  </Markdown>
-)
-
-const Hero = ({ data, tag: Tag = 'div', className = '', ...htmlProps }) => (
-  <Tag className={`${styles.hero} ${className}}`} {...htmlProps}>
-    <RootMilestone className={styles.top}>{data?.body}</RootMilestone>
-  </Tag>
-)
-
-// TODO: Fix `id` attribute not being rendered to html
 const HistoryPage = ({ data }) => (
   <Page
     title={data.title}
     description={data.description}
     className={styles.article}
   >
-    <div className={styles.bg}>
-      <img src={logo} alt="" className={styles['anniversary-logo']} />
-      <div className={styles.line} />
-    </div>
-    <div className={styles['page-container']}>
-      <Hero data={data} />
-      {data.milestones.slice(1).map((milestone) => (
-        <Milestone key={milestone.id} milestone={milestone} />
-      ))}
-      <RootMilestone className={styles.bottom} id="root">
-        {data.milestones[0]?.body}
-      </RootMilestone>
-    </div>
+    <section className={styles['front-section']}>
+      <Markdown className={`${styles['root-milestone']} ${styles.top}`}>
+        {data?.body}
+      </Markdown>
+    </section>
+    {data.milestones.slice(1).map((milestone) => (
+      <Milestone key={milestone.id} milestone={milestone} />
+    ))}
+    <section
+      className={`${styles['root-milestone']} ${styles.bottom}`}
+      id="root"
+    >
+      <Markdown>{data.milestones[0]?.body}</Markdown>
+    </section>
   </Page>
 )
 
