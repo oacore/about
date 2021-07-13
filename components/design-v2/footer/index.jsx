ceb9c732059c3eabc45c253f1702f458181c26c3
valerii <valerij.budko@gmail.com>
2021-07-13 10:35:33 +0300

    Add footer design v2

--- components/design-v2/footer/index.jsx
+++ components/design-v2/footer/index.jsx
@@ -15,49 +15,61 @@ const FooterDesign2 = ({
   <footer className={`${styles.footer} ${className}`}>
     <Container>
       <Row className={styles.footerHighlights}>
-        <Col md={{ size: 6, offset: 1 }} tag="aside">
-          <h4>Useful links</h4>
-          <ul className={styles.footerHighlightsList}>
-            {usefulLinks.map(({ title, path }) => (
-              <li key={`${title} @ ${path}`}>
-                <Link href={path}>{title}</Link>
-              </li>
-            ))}
-          </ul>
-        </Col>
-        <Col md="4">
-          <aside className={styles.footerCiteInfo}>
-            <h6>Writing about CORE?</h6>
-            <Markdown>{researchOutputs}</Markdown>
-          </aside>
+        <Col>Logo</Col>
+
+        {usefulLinks.map(({ titleBlock, menu }) => (
+          <Col>
+            <div>{titleBlock}</div>
+            <ul className={styles.footerHighlightsList}>
+              {menu.map(({ title, path }) => (
+                <li key={`${title} @ ${path}`}>
+                  <Link href={path}>{title}</Link>
+                </li>
+              ))}
+            </ul>
+          </Col>
+        ))}
+
+        <Col>
+          <div className={styles.footerCiteInfo}>
+            <div className={styles.columnLeft}>
+              <h6>Writing about CORE?</h6>
+              <Markdown>{researchOutputs}</Markdown>
+            </div>
+            <div>
+              <img src="/images/benefits/writing_about_core.svg" alt="Jisc" />
+            </div>
+          </div>
+
+          <div className={styles.footerPartners}>
+            <a
+              id="footer-logo-jisc"
+              className={`${styles.footerPartnersItem} ${styles.footerPartnersLogo}`}
+              href="https://www.jisc.ac.uk"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src="/images/logos/jisc.svg" alt="Jisc" />
+            </a>
+
+            <a
+              id="footer-logo-ou"
+              className={`${styles.footerPartnersItem} ${styles.footerPartnersLogo}`}
+              href="https://www.open.ac.uk"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src="images/logos/ou.svg" alt="The Open University" />
+            </a>
+
+            <Markdown
+              className={`${styles.footerPartnersItem} ${styles.footerPartnersText}`}
+            >
+              {partners}
+            </Markdown>
+          </div>
         </Col>
       </Row>
-
-      <div className={styles.footerPartners}>
-        <a
-          id="footer-logo-ou"
-          className={`${styles.footerPartnersItem} ${styles.footerPartnersLogo}`}
-          href="https://www.open.ac.uk"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src="/images/logos/ou.png" alt="The Open University" />
-        </a>
-        <a
-          id="footer-logo-jisc"
-          className={`${styles.footerPartnersItem} ${styles.footerPartnersLogo}`}
-          href="https://www.jisc.ac.uk"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src="/images/logos/jisc.svg" alt="Jisc" />
-        </a>
-        <Markdown
-          className={`${styles.footerPartnersItem} ${styles.footerPartnersText}`}
-        >
-          {partners}
-        </Markdown>
-      </div>
     </Container>
   </footer>
 )
@@ -65,8 +77,13 @@ const FooterDesign2 = ({
 FooterDesign2.propTypes = {
   usefulLinks: PropTypes.arrayOf(
     PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      path: PropTypes.string.isRequired,
+      titleBlock: PropTypes.string.isRequired,
+      menu: PropTypes.arrayOf(
+        PropTypes.shape({
+          title: PropTypes.string.isRequired,
+          path: PropTypes.string.isRequired,
+        })
+      ).isRequired,
     })
   ).isRequired,
   researchOutputs: PropTypes.string.isRequired,
--- main/index.jsx
+++ main/index.jsx
@@ -49,6 +49,9 @@ const Main = ({ children }) => {
   const cookiesAccepted = useCookie('cookies_accepted')
   const cookieHandler = useCookieHandler()
 
+  let footerConfig = config.footer
+  if (useRouter().route === '/benefits') footerConfig = config.footerV2
+
   return (
     <>
       <CookiesPopup
@@ -63,7 +66,7 @@ const Main = ({ children }) => {
         title={config.name}
         description={config.description}
         navigation={config.navigation}
-        footer={config.footer}
+        footer={footerConfig}
         activeRoute={router.route}
         onNavigate={handleNavigation}
       >
