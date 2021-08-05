75bc6d9c8762ad99208bb7821bfc49f282c13353
vkot91 <k.voskoboinik99@gmail.com>
2021-08-05 14:18:12 +0300

    Add dynamic data and improve styles

--- templates/community/research/index.jsx
+++ templates/community/research/index.jsx
@@ -1,42 +1,17 @@
-/* eslint-disable arrow-body-style */
-import { Card, Button, Carousel, Link } from '@oacore/design'
 import React from 'react'
+import { Card, Button } from '@oacore/design'
 import classNames from '@oacore/design/lib/utils/class-names'
 
 import styles from './research.module.scss'
 
-const cards = [
-  {
-    id: 1,
-    name: 'Daisuke Ikeda',
-    prof: 'Associate Professor at Kyushu University',
-    avatarUrl: '/images/community/research/share.svg',
-    text:
-      'I would be lost without CORE Researchers Community. I would like to personally thank you for the membership.',
-  },
-  {
-    id: 2,
-    name: 'Daisuke Ikeda2',
-    prof: 'Associate Professor at Kyushu University',
-    avatarUrl: '/images/community/research/share.svg',
-    text:
-      'I would be lost without CORE Researchers Community. I would like to personally thank you for the membership.',
-  },
-  {
-    id: 3,
-    name: 'Daisuke Ikeda3',
-    prof: 'Associate Professor at Kyushu University',
-    avatarUrl: '/images/community/research/share.svg',
-    text:
-      'I would be lost without CORE Researchers Community. I would like to personally thank you for the membership.',
-  },
-]
+import Markdown from 'components/markdown'
 
 const Research = ({ data }) => {
-  const { header, joinCards, mission, spotlight } = data
+  const { header, joinCards, mission, symposium, joinUs } = data
+
   return (
     <div className={styles.layout}>
-      <div className={classNames.use(styles.header, styles.container)}>
+      <section className={classNames.use(styles.header, styles.container)}>
         <div className={styles.headerText}>
           <h1 className={styles.title}>{header.title}</h1>
           <p>{header.citation}</p>
@@ -48,16 +23,23 @@ const Research = ({ data }) => {
             alt="Research Community"
           />
         </div>
-      </div>
+      </section>
       <section className={classNames.use(styles.contentMain, styles.container)}>
         <p className={styles.contentMainText}>{mission.description}</p>
         <Card className={styles.contentMainCard}>
           <span>ONLINE</span>
           <Card.Title tag="h5" className={styles.contentMainCardTitle}>
-            {spotlight.title}
+            {symposium.title}
           </Card.Title>
-          <Card.Description>{spotlight.description}</Card.Description>
-          <Button variant="contained">{spotlight.actionLabel}</Button>
+          <Card.Description>{symposium.description}</Card.Description>
+          <Button
+            variant="contained"
+            tag="a"
+            href="https://docs.google.com/forms/d/e/1FAIpQLSe-oyNDMTuZSrhktAjwo5Dyho1Jl2e11lOIo89oSlgYT7nD4A/viewform"
+            target="_blank"
+          >
+            {symposium.actionLabel}
+          </Button>
         </Card>
       </section>
       <section
@@ -88,16 +70,14 @@ const Research = ({ data }) => {
           </div>
         </div>
       </section>
-      <section className={classNames.use(styles.container, styles.community)}>
-        <h2 className={styles.title}>Researchers spotlight</h2>
-        <p>
-          Each month we focus on the recent work of one of the members of the
-          Researchers community. Here we take the opportunity to dive into some
-          of the diverse ways in which data from CORE is currently being used by
-          researchers around the world.
-        </p>
-        <div className={styles.communityCards}>
-          {cards.length > 2 && (
+
+      {/* <section className={classNames.use(styles.container,
+       styles.community)}>
+        <h2 className={styles.title}>{spotlight.title}</h2>
+        <p>{spotlight.description}</p>
+        <div className={styles.communityCards}> */}
+      {/* TODO: When we will have real quotes use this carousel. */}
+      {/* {cards.length > 2 && (
             <Carousel>
               {cards.map((card) => (
                 <Card key={card.id} className={styles.communityCard}>
@@ -119,22 +99,20 @@ const Research = ({ data }) => {
                 </Card>
               ))}
             </Carousel>
-          )}
-        </div>
-      </section>
-      <section className={styles.joinFooter}>
-        <h2 className={styles.title}>Join us</h2>
-        <p>
-          Far more than ‘yet another mailing list’, the CORE Researcher
-          community aims to be a catalyst, an active and participatory group
-          that exists to help its members gain the best from CORE and to learn
-          from the experiences of others.
-        </p>
-        <p className={styles.joinFooterContact}>
-          Contact us <Link href="https://core.ac.uk/about#contact">HERE</Link>{' '}
-          to request further details of how to join this group.
-        </p>
-        <Button variant="contained">Join now</Button>
+          )} */}
+      {/* </div>
+      </section> */}
+      <section className={classNames.use(styles.container, styles.joinFooter)}>
+        <h2 className={styles.title}>{joinUs.title}</h2>
+        <p>{joinUs.description}</p>
+        <Markdown>{joinUs.contact}</Markdown>
+        <Button
+          variant="contained"
+          href="https://core.ac.uk/about#contact"
+          tag="a"
+        >
+          {joinUs.actionLabel}
+        </Button>
       </section>
     </div>
   )
