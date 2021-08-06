cf1a000321675557b5a5cad67d57975613c0ab36
vkot91 <k.voskoboinik99@gmail.com>
2021-08-06 12:08:23 +0300

    Add new babel plugin to support new syntax

--- components/logo/index.jsx
+++ components/logo/index.jsx
@@ -3,21 +3,23 @@ import React from 'react'
 import logoPath from './core-symbol.svg'
 import styles from './logo.module.scss'
 
-const Logo = React.forwardRef((
-  {
-    text = 'CORE',
-    alt = text ? '' : 'CORE',
-    className = '',
-    tag: Tag = 'div',
-    ...restProps
-  },
-  // eslint-disable-next-line no-unused-vars
-  _
-) => (
-  <Tag className={`${styles.logo} ${className}`} {...restProps}>
-    <img src={logoPath} alt={alt} role={alt ? null : 'presentation'} />
-    {text}
-  </Tag>
-))
+const Logo = React.forwardRef(
+  (
+    {
+      text = 'CORE',
+      alt = text ? '' : 'CORE',
+      className = '',
+      tag: Tag = 'div',
+      ...restProps
+    },
+    // eslint-disable-next-line no-unused-vars
+    _
+  ) => (
+    <Tag className={`${styles.logo} ${className}`} {...restProps}>
+      <img src={logoPath} alt={alt} role={alt ? null : 'presentation'} />
+      {text}
+    </Tag>
+  )
+)
 
 export default Logo
