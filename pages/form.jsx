5ae7bf8055bda56458068e7db00c52fca1373511
Valeriy Budko <valerij.budko@gmail.com>
2021-10-15 13:30:00 +0300

    registration  (#436)
    * Revert "Create registration store and modals (#433)"
    
    This reverts commit 766740e47a08cf271f71b1ae69e2c4b83a23ab93.
    
    * Create registration store and modals (#433)
    
    * add Cypris logo
    
    * Create registration store and modals (#426)
    
    * Create registration store and modals
    
    * Fix scss warnings and update yml file
    
    * Add API_KEY for CI/CD
    
    * Add registration query and error component
    
    * Fix bug with regex match
    
    * Fix bug with modal window size
    
    * Add description text to modals
    
    * Change input styles
    
    * Change input placeholder style rule
    
    * Fix input placeholder text
    
    * Change font-size of input placeholder
    
    * Change modal text according to comments
    
    * Fix bug with country field
    
    * Improve error styles
    
    * Add files via upload
    
    Co-authored-by: CatherineKuliavets <60606796+CatherineKuliavets@users.noreply.github.com>
    Co-authored-by: Kostiantyn Voskoboinik <50716463+vkot91@users.noreply.github.com>
    
    Co-authored-by: CatherineKuliavets <60606796+CatherineKuliavets@users.noreply.github.com>
    Co-authored-by: Kostiantyn Voskoboinik <50716463+vkot91@users.noreply.github.com>

--- pages/form.jsx
+++ pages/form.jsx
@@ -28,9 +28,8 @@ export async function checkDataProviders({ params }) {
 }
 
 const AddDataProviderForm = React.forwardRef(({ onSubmit }, ref) => {
-  const [isIsDataProviderAddActive, setIsDataProviderAddActive] = useState(
-    false
-  )
+  const [isIsDataProviderAddActive, setIsDataProviderAddActive] =
+    useState(false)
   const {
     value: uri,
     element: elemDataProviderUrl,
