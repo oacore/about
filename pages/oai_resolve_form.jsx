e8f7b25a2f9a0dd9f868f2fd1be0fa1cc8e6e541
Valeriy Budko <valerij.budko@gmail.com>
2022-01-19 16:15:57 +0200

    Revert "Master update (#472)"
    This reverts commit c488022376e4a00bbe6dafadcb1e21a3a0af5d81.

--- pages/oai_resolve_form.jsx
+++ pages/oai_resolve_form.jsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react'
-import { TextField, Button, Link } from '@oacore/design'
-import { useRouter } from 'next/router'
+import React from 'react'
+import { TextField, Button } from '@oacore/design'
 
 import styles from './oai_resolve.module.scss'
 
@@ -13,41 +12,12 @@ const OAIResolveForm = React.forwardRef(({ onSubmit }, ref) => {
     bind: bindOAIidentifier,
   } = useInput('', 'oai-identifier')
 
-  const [isOAInotValid, setOAInotValid] = useState(false)
-
   const handleSubmit = (event) => {
-    event.preventDefault()
     const url = `https://api.core.ac.uk/oai/${event.target[0].value}`
-
-    const regexp = RegExp('(^oai:).+', 'g')
-    const matches = regexp.exec(bindOAIidentifier.value) !== null
-
     event.target.action = url
     if (onSubmit) onSubmit(event)
-
-    if (matches) event.currentTarget.submit()
-    else setOAInotValid(true)
   }
 
-  const errorMassageRoute = (
-    <>
-      The OAI identifier is not found.{' '}
-      <Link href="https://core.ac.uk/faq#oai-structure" target="_blank">
-        Here
-      </Link>{' '}
-      you can check all requirements.
-    </>
-  )
-
-  const errorMassageFormat = (
-    <>
-      The OAI format is wrong.{' '}
-      <Link href="https://core.ac.uk/faq#oai-structure" target="_blank">
-        Here
-      </Link>{' '}
-      you can check all requirements.
-    </>
-  )
   return (
     <>
       <form ref={ref} onSubmit={handleSubmit}>
@@ -55,12 +25,9 @@ const OAIResolveForm = React.forwardRef(({ onSubmit }, ref) => {
           id={elementOAIidentifier}
           name={elementOAIidentifier}
           label="Put OAI of the article"
+          // placeholder="For example, oai:oro.open.ac.uk:33975"
           value={OAIidentifier}
-          helper={
-            (useRouter().query.error === '404' && errorMassageRoute) ||
-            (isOAInotValid && errorMassageFormat)
-          }
-          variant="error"
+          // statusIcon
           required
           {...bindOAIidentifier}
         />
