3e8d899d40ceb170648a574c46b9e332a3e9dfe1
Kostiantyn Voskoboinik <k.voskoboinik99@gmail.com>
2022-02-03 08:21:26 +0200

    Update main page styles

--- pages/index.jsx
+++ pages/index.jsx
@@ -84,9 +84,8 @@ const IndexPage = () => (
           <SlideHeroItem key={slide.title} {...slide} />
         ))}
       </Carousel>
-
       <Section>
-        <h2 className="sr-only">{page.features.title}</h2>
+        <h2>{page.features.title}</h2>
         <KeyFeatureList>
           {page.features.children.map(
             ({ title, description, picture, status }) => (
@@ -115,7 +114,6 @@ const IndexPage = () => (
           ))}
         </JoinList>
       </Section>
-
       <Section className={styles.sectionTestimonial}>
         <h3 className={styles.title}>{page.testimonials.title}</h3>
         <TestimonialList>
@@ -129,7 +127,6 @@ const IndexPage = () => (
           </Button>
         </div>
       </Section>
-
       <Section className={styles.sectionServices}>
         <h3 className={styles.title}>{page.services.title}</h3>
         <ServicesList>
@@ -143,7 +140,6 @@ const IndexPage = () => (
           </Button>
         </div>
       </Section>
-
       <Section className={styles.sectionPartners}>
         <h3 className={styles.title}>{page.partnerProjects.title}</h3>
         <PartnerProjectsList />
