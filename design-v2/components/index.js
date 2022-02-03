ab34ab0fcf944d79492fcb0b34d01c7bfcbe911d
Kostiantyn Voskoboinik <k.voskoboinik99@gmail.com>
2022-02-03 08:15:08 +0200

    Update pages: about/endorsments,data,benefits

--- design-v2/components/index.js
+++ design-v2/components/index.js
@@ -1,4 +1,4 @@
 export * from './sections'
-export { default as Section } from './layout'
+export * from './layout'
 export { default as SearchForm } from './search-form'
 export { default as TestimonialCard } from './testimonial'
--- pages/index.jsx
+++ pages/index.jsx
@@ -16,6 +16,7 @@ import {
   TestimonialCard,
   ServicesList,
   ServiceItem,
+  Layout,
 } from 'design-v2/components'
 import { Page } from 'components'
 import page from 'data/home.yml'
@@ -76,82 +77,83 @@ const IndexPage = () => (
     title={page.title}
     description={page.description}
     keywords={page.keywords}
-    className={styles.page}
   >
-    <Carousel draggable={false} useArrows={false} slidesToShow={1} infinite>
-      {page.slides.children.map((slide) => (
-        <SlideHeroItem key={slide.title} {...slide} />
-      ))}
-    </Carousel>
-
-    <Section>
-      <h2 className="sr-only">{page.features.title}</h2>
-      <KeyFeatureList>
-        {page.features.children.map(
-          ({ title, description, picture, status }) => (
-            <KeyFeature
-              title={title}
-              status={status}
-              icon={picture}
-              key={title}
-            >
-              {description}
-            </KeyFeature>
-          )
-        )}
-      </KeyFeatureList>
-    </Section>
-    <Section className={styles.sectionJoin}>
-      <h3 className={styles.title}>{page.join.title}</h3>
-      <JoinList className={styles.sectionJoinList}>
-        {page.join.children.map(({ caption, url, picture }) => (
-          <JoinItem
-            caption={caption}
-            url={url}
-            picture={picture}
-            key={caption}
-          />
+    <Layout>
+      <Carousel draggable={false} useArrows={false} slidesToShow={1} infinite>
+        {page.slides.children.map((slide) => (
+          <SlideHeroItem key={slide.title} {...slide} />
         ))}
-      </JoinList>
-    </Section>
+      </Carousel>
 
-    <Section className={styles.sectionTestimonial}>
-      <h3 className={styles.title}>{page.testimonials.title}</h3>
-      <TestimonialList>
-        {page.testimonials.children.map((item) => (
-          <TestimonialItem key={item.title} {...item} />
-        ))}
-      </TestimonialList>
-      <div className={styles.actionButton}>
-        <Button variant="contained" tag="a" href={page.testimonials.action}>
-          {page.testimonials.actionLabel}
-        </Button>
-      </div>
-    </Section>
+      <Section>
+        <h2 className="sr-only">{page.features.title}</h2>
+        <KeyFeatureList>
+          {page.features.children.map(
+            ({ title, description, picture, status }) => (
+              <KeyFeature
+                title={title}
+                status={status}
+                icon={picture}
+                key={title}
+              >
+                {description}
+              </KeyFeature>
+            )
+          )}
+        </KeyFeatureList>
+      </Section>
+      <Section className={styles.sectionJoin}>
+        <h3 className={styles.title}>{page.join.title}</h3>
+        <JoinList className={styles.sectionJoinList}>
+          {page.join.children.map(({ caption, url, picture }) => (
+            <JoinItem
+              caption={caption}
+              url={url}
+              picture={picture}
+              key={caption}
+            />
+          ))}
+        </JoinList>
+      </Section>
 
-    <Section className={styles.sectionServices}>
-      <h3 className={styles.title}>{page.services.title}</h3>
-      <ServicesList>
-        {page.services.children.map((item) => (
-          <ServiceItem key={item.title} {...item} tag="a" />
-        ))}
-      </ServicesList>
-      <div className={styles.actionButton}>
-        <Button variant="contained" tag="a" href={page.services.action}>
-          {page.services.actionLabel}
-        </Button>
-      </div>
-    </Section>
+      <Section className={styles.sectionTestimonial}>
+        <h3 className={styles.title}>{page.testimonials.title}</h3>
+        <TestimonialList>
+          {page.testimonials.children.map((item) => (
+            <TestimonialItem key={item.title} {...item} />
+          ))}
+        </TestimonialList>
+        <div className={styles.actionButton}>
+          <Button variant="contained" tag="a" href={page.testimonials.action}>
+            {page.testimonials.actionLabel}
+          </Button>
+        </div>
+      </Section>
 
-    <Section className={styles.sectionPartners}>
-      <h3 className={styles.title}>{page.partnerProjects.title}</h3>
-      <PartnerProjectsList />
-      <div className={styles.actionButton}>
-        <Button variant="outlined" tag="a" href={page.partnerProjects.action}>
-          {page.partnerProjects.actionLabel}
-        </Button>
-      </div>
-    </Section>
+      <Section className={styles.sectionServices}>
+        <h3 className={styles.title}>{page.services.title}</h3>
+        <ServicesList>
+          {page.services.children.map((item) => (
+            <ServiceItem key={item.title} {...item} tag="a" />
+          ))}
+        </ServicesList>
+        <div className={styles.actionButton}>
+          <Button variant="contained" tag="a" href={page.services.action}>
+            {page.services.actionLabel}
+          </Button>
+        </div>
+      </Section>
+
+      <Section className={styles.sectionPartners}>
+        <h3 className={styles.title}>{page.partnerProjects.title}</h3>
+        <PartnerProjectsList />
+        <div className={styles.actionButton}>
+          <Button variant="outlined" tag="a" href={page.partnerProjects.action}>
+            {page.partnerProjects.actionLabel}
+          </Button>
+        </div>
+      </Section>
+    </Layout>
   </Page>
 )
 
