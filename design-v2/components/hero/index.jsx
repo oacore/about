e081f929b652cd8748766aac1a0358ef6d6a27e3
Kostiantyn Voskoboinik <k.voskoboinik99@gmail.com>
2022-02-11 10:26:08 +0200

    Update services page

---
+++ design-v2/components/hero/index.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Card, Button } from '@oacore/design/lib/elements'
+import { classNames } from '@oacore/design/lib/utils'
+
+import SearchForm from '../search-form'
+import styles from './styles.module.scss'
+
+const Hero = ({
+  title,
+  image: imgHref,
+  label,
+  description,
+  action,
+  actionLabel,
+}) => (
+  <div className={styles.heroItem}>
+    <Card variant="pure" className={styles.heroItemCard}>
+      <Card.Title tag="h2" className={styles.heroTitle}>
+        {title}
+      </Card.Title>
+      <Card.Description tag="div">
+        {action === 'Search' ? (
+          <SearchForm />
+        ) : (
+          <>
+            {description && (
+              <p className={styles.heroItemDescription}>{description}</p>
+            )}
+            {actionLabel && (
+              <Button tag="a" variant="contained" href={action}>
+                {actionLabel}
+              </Button>
+            )}
+          </>
+        )}
+      </Card.Description>
+    </Card>
+    <img
+      src={imgHref}
+      alt={label}
+      className={classNames.use(styles.heroItemImg, {
+        [styles.heroItemImgBig]: action === 'Search',
+      })}
+    />
+  </div>
+)
+
+export default Hero
