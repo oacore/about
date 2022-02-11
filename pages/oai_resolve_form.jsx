57329e3b3ea3183f02145bce50d98cd3d572774e
valerii <valerij.budko@gmail.com>
2022-02-11 17:36:16 +0200

    Fix form input

--- pages/oai_resolve_form.jsx
+++ pages/oai_resolve_form.jsx
@@ -1,35 +1,56 @@
+/* eslint-disable no-unused-vars */
 import React, { useState } from 'react'
-import { TextField, Button, Link } from '@oacore/design'
+import { TextField, Button, Link, Select } from '@oacore/design'
 import { useRouter } from 'next/router'
 
 import styles from './oai_resolve.module.scss'
 
 import { useInput } from 'hooks'
 
+const validateIdentifier = (string) => {
+  const regexp = RegExp('(^oai:).+', 'g')
+
+  if (regexp.exec('oai_resolver') !== null) return true
+
+  return regexp.exec(string) !== null
+}
+
 const OAIResolveForm = React.forwardRef(({ onSubmit }, ref) => {
+  const router = useRouter()
+  const identifier = router.route.substring(1)
+
   const {
     value: OAIidentifier,
     element: elementOAIidentifier,
     bind: bindOAIidentifier,
-  } = useInput('', 'oai-identifier')
+    onInput: onInputIidentifier,
+  } = useInput(
+    validateIdentifier(identifier) ? identifier : '',
+    'oai-identifier'
+  )
+  const [value, setValue] = useState('')
+
+  const handleInput = (e) => {
+    setValue(e.value)
+  }
 
   const [isOAInotValid, setOAInotValid] = useState(false)
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    const url = `https://api.core.ac.uk/oai/${event.target[0].value}`
 
-    const regexp = RegExp('(^oai:).+', 'g')
-    const matches = regexp.exec(bindOAIidentifier.value) !== null
+    const valueInput = event.target[1].value
 
-    event.target.action = url
+    event.target.action = `https://api.core.ac.uk/oai/${valueInput}`
     if (onSubmit) onSubmit(event)
 
-    if (matches) event.currentTarget.submit()
-    else setOAInotValid(true)
+    if (validateIdentifier(valueInput)) {
+      setOAInotValid(false)
+      event.currentTarget.submit()
+    } else setOAInotValid(true)
   }
 
-  const errorMassageRoute = (
+  const msgErrorRoute = (
     <>
       The OAI identifier is not found.{' '}
       <Link href="https://core.ac.uk/faq#oai-structure" target="_blank">
@@ -39,7 +60,7 @@ const OAIResolveForm = React.forwardRef(({ onSubmit }, ref) => {
     </>
   )
 
-  const errorMassageFormat = (
+  const msgErrorFormat = (
     <>
       The OAI format is wrong.{' '}
       <Link href="https://core.ac.uk/faq#oai-structure" target="_blank">
@@ -48,22 +69,39 @@ const OAIResolveForm = React.forwardRef(({ onSubmit }, ref) => {
       you can check all requirements.
     </>
   )
+
+  const msgHelper = (
+    <>
+      Example:{' '}
+      <Link
+        href="https://core.ac.uk/display/12820815?recSetID="
+        target="_blank"
+        className={styles.helperLink}
+      >
+        oai:researchonline.rca.ac.uk:1035
+      </Link>
+    </>
+  )
+
   return (
     <>
       <form ref={ref} onSubmit={handleSubmit}>
-        <TextField
-          id={elementOAIidentifier}
-          name={elementOAIidentifier}
-          label="Put OAI of the article"
+        <Select
+          id="elementOAIidentifier"
           value={OAIidentifier}
-          helper={
-            (useRouter().query.error === '404' && errorMassageRoute) ||
-            (isOAInotValid && errorMassageFormat)
-          }
-          variant="error"
-          required
+          variant={isOAInotValid ? 'normal' : 'pure'}
+          prependIcon="#magnify"
+          placeholder="Put OAI of the article"
+          changeOnBlur={false}
+          onInput={handleInput}
+          className={styles.select}
+          label=""
           {...bindOAIidentifier}
         />
+        <span className={styles.helper}>
+          {isOAInotValid ? msgErrorFormat : msgHelper}
+        </span>
+
         <div className={styles.submitSection}>
           <Button
             className={styles.buttonCustom}
