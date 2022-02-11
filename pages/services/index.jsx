fdee9d107fc0340cb7a4d74c9059e066f87e7427
Kostiantyn Voskoboinik <50716463+vkot91@users.noreply.github.com>
2022-02-11 16:13:14 +0200

    Core 4184/implement services page (#507)
    * Change static text
    
    * Update services page
    
    * Update styles
    
    * Update margins and paddings

--- pages/services/index.jsx
+++ pages/services/index.jsx
@@ -1,19 +1,76 @@
 import React from 'react'
-import { Row, Col } from 'reactstrap'
+import { Card, Button } from '@oacore/design/lib/elements'
 
 import styles from './services.module.scss'
 
-import {
-  Page,
-  Content,
-  Markdown,
-  Section,
-  Button,
-  Link,
-  Video,
-} from 'components'
-import Testimonial from 'components/testimonial'
+import { Page } from 'components'
 import servicesData from 'data/services.yml'
+import { Hero, Layout, Section, Video } from 'design-v2/components'
+
+const FeatureBox = ({ iconSrc, title, description, recommender, action }) => (
+  <Card variant="pure" className={styles.card}>
+    <img src={iconSrc} alt={title} className={styles.cardLogo} />
+    <Card.Title tag="h5">{title}</Card.Title>
+    <Card.Description>{description}</Card.Description>
+    <Card.Footer className={styles.cardFooter}>
+      <span className={styles.cardFooterCaption}>Recommender for:</span>
+      <div className={styles.cardRecommender}>
+        {recommender.map((item) => (
+          <div key={item.title} className={styles.item}>
+            {item.title}
+          </div>
+        ))}
+      </div>
+      <Button variant="contained" href={action.url} tag="a">
+        {action.caption}
+      </Button>
+    </Card.Footer>
+  </Card>
+)
+
+const FeaturesSection = ({ description, title, video, features }) => {
+  const [visibleVideo, setVisibleVideo] = React.useState(false)
+
+  return (
+    <Section className={styles.sectionContainer}>
+      <div className={styles.sectionHeader}>
+        {video && (
+          <button
+            className={styles.sectionVideoImg}
+            type="button"
+            onClick={() => setVisibleVideo(true)}
+          >
+            <img src="/images/services/video_placeholder.svg" alt="video" />
+          </button>
+        )}
+
+        <p hidden>{description}</p>
+        <h3 className={styles.sectionTitle}>{title}</h3>
+
+        {video && (
+          <Video
+            visibleModal={visibleVideo}
+            closeModal={() => setVisibleVideo(false)}
+            video={video}
+          />
+        )}
+      </div>
+      <div className={styles.features}>
+        {features &&
+          features.map((feature) => (
+            <FeatureBox
+              key={feature.id}
+              iconSrc={feature.logo}
+              title={feature.title}
+              description={feature.description}
+              recommender={feature.recommender}
+              action={feature.action}
+            />
+          ))}
+      </div>
+    </Section>
+  )
+}
 
 const ServicesPage = () => (
   <Page
@@ -22,109 +79,24 @@ const ServicesPage = () => (
     keywords={servicesData.keywords}
     nav
   >
-    <h1 className={styles['services-page-title']}>
-      <span className={styles['services-page-title-small']}>
-        {servicesData.headline[0]}
-      </span>
-      {servicesData.headline[1]}
-    </h1>
-
-    {servicesData.testimonial && (
-      <Testimonial
-        className="my-5"
-        content={servicesData.testimonial.content}
-        author={servicesData.testimonial.author}
+    <Layout>
+      <Hero
+        image={servicesData.hero}
+        title={servicesData.title}
+        description={servicesData.description}
       />
-    )}
-
-    {servicesData.sections.map((servicesGroup) => (
-      <Section
-        key={servicesGroup.id}
-        id={servicesGroup.id}
-        caption={servicesGroup.title}
-        className={styles['service-section']}
-        container={false}
-      >
-        <h2 className="text-center">{servicesGroup.title}</h2>
-
-        {servicesGroup.video && (
-          <Section className={styles['service-section-video']} tag="div">
-            <Content className="mx-auto" tag="figure">
-              <Content tag="figcaption">
-                <Markdown>{servicesGroup.video.description}</Markdown>
-              </Content>
-              <Video
-                src={servicesGroup.video.src}
-                title={servicesGroup.video.title}
-              />
-            </Content>
-          </Section>
-        )}
-
-        {servicesGroup.sections.map((service) => (
-          <Section
-            key={service.id}
-            id={service.id}
-            className={styles['service-section']}
-          >
-            <Row className={styles['service-section-title']} tag="h3">
-              <Link href={service.action.url} passHref>
-                <Col
-                  href={service.action.url}
-                  title={service.action.caption}
-                  className={styles['service-section-logo']}
-                  sm="6"
-                  md="4"
-                  tag="a"
-                >
-                  <img src={service.logo} alt={`${service.title}'s logo`} />
-                </Col>
-              </Link>
-
-              <Col sm="6" md="8" tag="span">
-                {service.title}
-              </Col>
-            </Row>
-
-            <Row className={styles['service-section-content']}>
-              <Col sm="6" md="4">
-                {service.screenshot && (
-                  <figure>
-                    <Link href={service.action.url} passHref>
-                      <a
-                        href={service.action.url}
-                        title={service.action.caption}
-                      >
-                        <img
-                          className={styles['service-section-screenshot']}
-                          src={service.screenshot}
-                          alt={`${service.title}'s screenshot`}
-                        />
-                      </a>
-                    </Link>
-                  </figure>
-                )}
-              </Col>
-
-              <Col sm="6" md="8">
-                <Content
-                  markdown
-                  id={service.description}
-                  key={service.description}
-                >
-                  {service.description}
-                </Content>
-                <footer className={styles['service-section-footer']}>
-                  <Button color="primary" outline href={service.action.url}>
-                    {service.action.caption}
-                  </Button>
-                </footer>
-              </Col>
-            </Row>
-          </Section>
+      <div className={styles.wrapper}>
+        {servicesData.sections.map((item) => (
+          <FeaturesSection
+            id={item.id}
+            description={item.description}
+            title={item.title}
+            video={item.video}
+            features={item.features}
+          />
         ))}
-      </Section>
-    ))}
+      </div>
+    </Layout>
   </Page>
 )
 
