74da3ad84e22344c50c6d2d3fc1dfe4533a71f6d
Valeriy Budko <valerij.budko@gmail.com>
2022-02-14 16:24:55 +0200

    CORE-4174 Update OAI resolver (#508)
    * update text
    
    * Fix form input
    
    * FAQ update OAI
    
    * FAQ update OAI
    
    * FAQ update OAI
    
    * FAQ update OAI
    
    * Fix column padding

--- pages/oai_resolve_form.jsx
+++ pages/oai_resolve_form.jsx
@@ -1,69 +1,120 @@
+/* eslint-disable no-nested-ternary */
+/* eslint-disable no-unused-vars */
 import React, { useState } from 'react'
-import { TextField, Button, Link } from '@oacore/design'
+import { TextField, Button, Link, Select } from '@oacore/design'
 import { useRouter } from 'next/router'
 
 import styles from './oai_resolve.module.scss'
 
 import { useInput } from 'hooks'
 
+const validateIdentifier = (string) => {
+  const regexp = RegExp('(^oai:).+', 'g')
+
+  if (regexp.exec('oai_resolver') !== null) return true
+
+  return regexp.exec(string) !== null
+}
+
 const OAIResolveForm = React.forwardRef(({ onSubmit }, ref) => {
+  const router = useRouter()
+  const identifier = router.route.substring(1)
+  const { error } = router.query
+
   const {
     value: OAIidentifier,
     element: elementOAIidentifier,
     bind: bindOAIidentifier,
-  } = useInput('', 'oai-identifier')
+    onInput: onInputIidentifier,
+  } = useInput(
+    validateIdentifier(identifier) ? identifier : '',
+    'oai-identifier'
+  )
+  const [value, setValue] = useState('')
+
+  const handleInput = (e) => {
+    setValue(e.value)
+  }
 
   const [isOAInotValid, setOAInotValid] = useState(false)
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    const url = `https://api.core.ac.uk/oai/${event.target[0].value}`
 
-    const regexp = RegExp('(^oai:).+', 'g')
-    const matches = regexp.exec(bindOAIidentifier.value) !== null
+    const valueInput = event.target[1].value
 
-    event.target.action = url
+    event.target.action = `https://api.core.ac.uk/oai/${valueInput}`
     if (onSubmit) onSubmit(event)
 
-    if (matches) event.currentTarget.submit()
-    else setOAInotValid(true)
+    if (validateIdentifier(valueInput)) {
+      setOAInotValid(false)
+      event.currentTarget.submit()
+    } else setOAInotValid(true)
   }
 
-  const errorMassageRoute = (
+  const msgErrorRoute = (
     <>
-      The OAI identifier is not found.{' '}
-      <Link href="https://core.ac.uk/faq#oai-structure" target="_blank">
+      <span>
+        We can not find this OAI identifier.
+        <br />
+      </span>
+      <span>
+        If you believe that this OAI identifier is registered with a repository,
+        check the reasons why this might be the case in our{'  '}
+        <Link href="https://core.ac.uk/faq#oai-about" target="_blank">
+          FAQ
+        </Link>
+        .
+      </span>
+    </>
+  )
+
+  const msgErrorFormat = (
+    <>
+      The OAI format is wrong.{' '}
+      <Link href="https://core.ac.uk/faq#oai-about" target="_blank">
         Here
       </Link>{' '}
       you can check all requirements.
     </>
   )
 
-  const errorMassageFormat = (
+  const msgHelper = (
     <>
-      The OAI format is wrong.{' '}
-      <Link href="https://core.ac.uk/faq#oai-structure" target="_blank">
-        Here
-      </Link>{' '}
-      you can check all requirements.
+      Example:{' '}
+      <Link
+        href="https://core.ac.uk/display/12820815?recSetID="
+        target="_blank"
+        className={styles.helperLink}
+      >
+        oai:researchonline.rca.ac.uk:1035
+      </Link>
     </>
   )
+
   return (
     <>
       <form ref={ref} onSubmit={handleSubmit}>
-        <TextField
-          id={elementOAIidentifier}
-          name={elementOAIidentifier}
-          label="Put OAI of the article"
+        <Select
+          id="elementOAIidentifier"
           value={OAIidentifier}
-          helper={
-            (useRouter().query.error === '404' && errorMassageRoute) ||
-            (isOAInotValid && errorMassageFormat)
-          }
-          variant="error"
-          required
+          variant={isOAInotValid ? 'normal' : 'pure'}
+          prependIcon="#magnify"
+          placeholder="Put OAI of the article"
+          changeOnBlur={false}
+          onInput={handleInput}
+          className={styles.select}
+          label=""
           {...bindOAIidentifier}
         />
+        <span className={styles.helper}>
+          {isOAInotValid
+            ? msgErrorFormat
+            : error === '404'
+            ? msgErrorRoute
+            : msgHelper}
+        </span>
+
         <div className={styles.submitSection}>
           <Button
             className={styles.buttonCustom}
