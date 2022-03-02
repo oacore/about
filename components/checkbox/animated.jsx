bf08e2f3a423176ef312f3926091666f4de2472b
Valeriy Budko <valerij.budko@gmail.com>
2022-03-02 11:53:07 +0200

    Core 4194 checkbox to api/dataset registration form (#537)
    * CORE-4174 Update OAI resolver (#508)
    
    * update text
    
    * Fix form input
    
    * FAQ update OAI
    
    * FAQ update OAI
    
    * FAQ update OAI
    
    * FAQ update OAI
    
    * Fix column padding
    
    * Update serch box text and services box (#517)
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Delete Founder Chorst Klaus.jpg
    
    * Oai resolver assets (#534)
    
    * CORE-4174 Update OAI resolver (#508)
    
    * update text
    
    * Fix form input
    
    * FAQ update OAI
    
    * FAQ update OAI
    
    * FAQ update OAI
    
    * FAQ update OAI
    
    * Fix column padding
    
    * Update serch box text and services box (#517)
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Delete Founder Chorst Klaus.jpg
    
    * oai assets path
    
    * style fix
    
    * image prefix fix
    
    Co-authored-by: Kostiantyn Voskoboinik <50716463+vkot91@users.noreply.github.com>
    Co-authored-by: Viktoriiapavlenko04 <86836995+Viktoriiapavlenko04@users.noreply.github.com>
    
    * add animated checkbox
    
    * update styles
    
    * set module format
    
    * upd package
    
    * pack
    
    Co-authored-by: Kostiantyn Voskoboinik <50716463+vkot91@users.noreply.github.com>
    Co-authored-by: Viktoriiapavlenko04 <86836995+Viktoriiapavlenko04@users.noreply.github.com>

---
+++ components/checkbox/animated.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react'
+import {
+  animated,
+  useSpring,
+  config,
+  useSpringRef,
+  useChain,
+} from 'react-spring'
+
+import styles from './styles.module.scss'
+
+const Checkbox = ({ id, labelText, setCheckbox }) => {
+  const [isChecked, setIsChecked] = useState(false)
+  const checkboxAnimationRef = useSpringRef()
+  const checkboxAnimationStyle = useSpring({
+    backgroundColor: isChecked ? '#b75400' : '#fff',
+    borderColor: isChecked ? '#b75400' : '#757575',
+    config: config.gentle,
+    ref: checkboxAnimationRef,
+  })
+
+  const [checkmarkLength, setCheckmarkLength] = useState(null)
+
+  const checkmarkAnimationRef = useSpringRef()
+  const checkmarkAnimationStyle = useSpring({
+    x: isChecked ? 0 : checkmarkLength,
+    config: config.gentle,
+    ref: checkmarkAnimationRef,
+  })
+
+  useChain(
+    isChecked
+      ? [checkboxAnimationRef, checkmarkAnimationRef]
+      : [checkmarkAnimationRef, checkboxAnimationRef],
+    [0, 0.1]
+  )
+
+  return (
+    // eslint-disable-next-line jsx-a11y/label-has-associated-control
+    <label className={styles.labelBox}>
+      <input
+        id={id}
+        type="checkbox"
+        onChange={() => {
+          setIsChecked(!isChecked)
+          setCheckbox(!isChecked)
+        }}
+        className={styles.disabled}
+      />
+      <animated.svg
+        style={checkboxAnimationStyle}
+        className={styles.checkbox}
+        aria-hidden="true"
+        viewBox="0 0 15 11"
+        fill="none"
+      >
+        <animated.path
+          d="M1 4.5L5 9L14 1"
+          strokeWidth="2"
+          stroke="#fff"
+          ref={(ref) => {
+            if (ref) setCheckmarkLength(ref.getTotalLength())
+          }}
+          strokeDasharray={checkmarkLength}
+          strokeDashoffset={checkmarkAnimationStyle.x}
+        />
+      </animated.svg>
+      <div className={styles.label}>{labelText}</div>
+    </label>
+  )
+}
+
+export default Checkbox
