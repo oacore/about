f08be3441c366015669f1feb0c4195827f52d499
valerii <valerij.budko@gmail.com>
2022-03-27 14:45:47 +0300

    CORE-4226 Set base page OAI docs

---
+++ pages/documentation/oai-resolver.jsx
@@ -0,0 +1,22 @@
+import retrieveContent from 'content'
+import { DocumentationPageOaiResolver } from 'templates'
+
+const PAGES_BASE = 'docs'
+const PAGE_SLUG = 'oai-resolver'
+
+async function getStaticProps({ previewData }) {
+  const ref = previewData?.ref
+  const filepath = `${PAGES_BASE}/${PAGE_SLUG}.md`
+  const data = await retrieveContent(filepath, { ref })
+
+  return {
+    props: {
+      data,
+    },
+  }
+}
+
+// This bare import could be composed with a page created here
+// if more props needed to process
+export default DocumentationPageOaiResolver
+export { getStaticProps }
