a22970caf2bcab08122ab10bdd464914a82f827f
Kostiantyn Voskoboinik <k.voskoboinik99@gmail.com>
2022-05-30 12:29:41 +0300

    Update error page text

--- design-v2/components/error/index.jsx
+++ design-v2/components/error/index.jsx
@@ -1,16 +1,18 @@
+import React from 'react'
 import { Button } from '@oacore/design/lib/elements'
 import { classNames } from '@oacore/design/lib/utils'
-import React from 'react'
 
 import Layout from '../layout/layout'
 import styles from './styles.module.scss'
+
+import Markdown from 'components/markdown'
 
 const ErrorTemplate = ({ text, className }) => (
   <Layout>
     <div className={classNames.use(styles.container).join(className)}>
       <img src={text.image.url} alt={text.image.caption} />
       <h4>{text.title}</h4>
-      <p>{text.description}</p>
+      <Markdown>{text.description}</Markdown>
       <div className={styles.group}>
         {text.actions.map(({ caption, url, variant }) => (
           <Button key={caption} href={url} variant={variant || 'contained'}>
