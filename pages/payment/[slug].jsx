d87ae14421f3a9d82523014fc4f245b25de71ec6
vbudko <valerij.budko@gmail.com>
2022-10-18 12:30:54 +0300

    [CORE-4379] membership plan #3

---
+++ pages/payment/[slug].jsx
@@ -0,0 +1,69 @@
+import React, {useEffect} from 'react'
+
+import {Page} from 'components'
+import textData from 'data/payment.yml'
+import PaymentPageTemplate from 'templates/payment'
+import {useStore} from 'store'
+
+export const slugs = ['starting', 'supporting', 'sustaining']
+
+export async function getStaticProps({ params }) {
+  const { slug } = params
+
+  const data = {
+    // header: {
+    //   title: textData.header.title,
+    //   description: textData.fee.description[slug],
+    // },
+    // fee: {
+    //   ...textData.fee,
+    //   table: {
+    //     headers: textData.fee.table.headers,
+    //     ...textData.fee.table[slug],
+    //   },
+    // },
+    planName: slug,
+    // comparisonTable: textData.comparisonTable,
+    // box: textData.box,
+    // discount: textData.discount,
+  }
+
+  return {
+    props: {
+      data,
+    },
+  }
+}
+
+export async function getStaticPaths() {
+  const paths = slugs.map((slug) => ({
+    params: { slug },
+  }))
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+const PaymentPage = ({data}) => {
+  const {membership} = useStore()
+  const { planName } = data
+
+  console.log("PaymentPage planName " + planName)
+
+  React.useEffect(() => {
+    membership.setData({
+      planName
+    })
+  }, [])
+
+  // return <>1</>
+  return (
+    <Page title={textData.shortTitle} description={textData.caption}>
+      <PaymentPageTemplate textData={textData}/>
+    </Page>
+  )
+}
+
+export default PaymentPage
