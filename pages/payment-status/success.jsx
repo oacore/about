ca87158a31ad7eb6d18af3c8f9615e56402df082
vbudko <valerij.budko@gmail.com>
2022-10-19 16:41:08 +0300

    [CORE-4379] Fix status page

--- pages/payment-status/success.jsx
+++ pages/payment-status/success.jsx
@@ -1,28 +1,26 @@
 import React from 'react'
-import { useRouter } from 'next/router'
+// import { useRouter } from 'next/router'
 
 import PaymentSuccessPageTemplate from 'templates/payment/success'
 import { Page } from 'components'
 import textData from 'data/payment.yml'
-import { useStore } from 'store'
+// import { useStore } from 'store'
 
-const PaymentSuccessPage = () => {
-  const { membership } = useStore()
-  const router = useRouter()
-  const [loaded, setLoaded] = React.useState(false)
+const PaymentSuccessPage = () => (
+  // const { membership } = useStore()
+  // const router = useRouter()
+  // const [loaded, setLoaded] = React.useState(false)
 
-  React.useEffect(() => {
-    if (membership.data.planName === '') router.push('/membership')
-    else setLoaded(true)
-  }, [])
+  // React.useEffect(() => {
+  //   if (membership.data.planName === '') router.push('/membership')
+  //   else setLoaded(true)
+  // }, [])
+  //
+  // if (!loaded) return <div />
 
-  if (!loaded) return <div />
-
-  return (
-    <Page title={textData.shortTitle} description={textData.caption}>
-      <PaymentSuccessPageTemplate textData={textData.success} />
-    </Page>
-  )
-}
+  <Page title={textData.shortTitle} description={textData.caption}>
+    <PaymentSuccessPageTemplate textData={textData.success} />
+  </Page>
+)
 
 export default PaymentSuccessPage
