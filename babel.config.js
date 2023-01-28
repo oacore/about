02c577614a5760c62dce8476f5a08b77215d01d7
vbudko <valerij.budko@gmail.com>
2023-01-28 21:13:41 +0000

    [bugfix] Config babel/runtime

--- babel.config.js
+++ babel.config.js
@@ -23,7 +23,6 @@ module.exports = (api) => {
     '@babel/plugin-syntax-dynamic-import',
     '@babel/plugin-proposal-optional-chaining',
     '@babel/plugin-proposal-nullish-coalescing-operator',
-    '@babel/plugin-transform-runtime',
     '@babel/runtime',
   ]
 
