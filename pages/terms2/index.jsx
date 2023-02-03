bac1c10273b6312dd016f51845a84573bd93bf49
Eka <ekachxaidze2@gmail.com>
2023-02-03 14:41:03 +0400

    temporary page

--- pages/terms2/index.jsx
+++ pages/terms2/index.jsx
@@ -4,10 +4,10 @@ import textData from '../../data/terms.yml'
 import { Page } from '../../components'
 import TermsPageTemplate from '../../templates/term'
 
-const TermsPage = () => (
+const TermsPage2 = () => (
   <Page title={textData.header.title} description={textData.header.description}>
     <TermsPageTemplate data={textData} />
   </Page>
 )
 
-export default TermsPage
+export default TermsPage2
