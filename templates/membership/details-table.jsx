ab36dbd4e4d8d37c4df3add4e6a35b905fd25abd
ekachxaidze98 <65679299+ekachxaidze98@users.noreply.github.com>
2023-02-07 17:12:51 +0400

    Core 4460 update membership page (#786)
    * update dataset text
    
    * CORE-4460 update membership page
    
    * data.box undefined issue
    
    * data.box undefined issue
    
    * update box

--- templates/membership/details-table.jsx
+++ templates/membership/details-table.jsx
@@ -1,21 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Button } from '@oacore/design/lib/elements'
 
 import styles from './styles.module.scss'
 import useTable from './hooks/useTable'
+import { Markdown } from '../../components'
 
 import { MembershipTable, Section, TextBox } from 'design-v2/components'
 
-const DetailsTable = ({ data }) => {
+const DetailsTable = ({ data, howItWorksOption }) => {
   const { visibleTable, toggleVisibleTable } = useTable()
+  const [pathType, setPathType] = useState(false)
+
+  useEffect(() => {
+    const path = window?.location?.pathname
+    setPathType(path.includes('/membership/'))
+  }, [])
+
   return (
-    <Section id="comparison-table">
-      <TextBox
-        onClick={toggleVisibleTable}
-        description={data.box.description}
-        buttonCaption={
-          visibleTable ? data.box.action.active : data.box.action.default
-        }
-      />
+    <Section id={howItWorksOption ? 'documentation' : 'comparison-table'}>
+      {howItWorksOption || !pathType ? (
+        <Section id="how-it-works" className={styles.howItWorks}>
+          <div className={styles.imageWrapper}>
+            <img src={data.howItWorks?.image} alt={data.howItWorks?.title} />
+          </div>
+          <article className={styles.content}>
+            <h3>{data.howItWorks?.title}</h3>
+            <Markdown>{data.howItWorks?.description}</Markdown>
+            {data.howItWorks?.action && (
+              <Button
+                className={styles.button}
+                href={data.howItWorks?.action.url}
+                variant={data.howItWorks?.action.variant || 'outlined'}
+                onClick={toggleVisibleTable}
+              >
+                {visibleTable
+                  ? data.howItWorks?.action.active
+                  : data.howItWorks?.action.default}
+              </Button>
+            )}
+          </article>
+        </Section>
+      ) : (
+        <TextBox
+          onClick={toggleVisibleTable}
+          description={data?.box?.description}
+          buttonCaption={
+            visibleTable
+              ? data?.box?.action?.active
+              : data?.box?.action?.default
+          }
+        />
+      )}
       {visibleTable && (
         <MembershipTable
           className={styles.table}
