84fae29334a4fbd9efe2dbe0ede4e6ad619baa82
Eka <ekachxaidze2@gmail.com>
2023-02-15 14:25:59 +0400

    component

--- templates/membership-documentation/index.jsx
+++ templates/membership-documentation/index.jsx
@@ -1,53 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {
+  DocumentationMembership,
+  DocumentationMembershipNav,
+} from '@oacore/design/src'
 
-// import Markdown from '../../components/markdown';
+import Markdown from '../../components/markdown'
 import { Layout } from '../../design-v2/components'
-// import {DocumentationMembership, DocumentationMembershipNav}
-// from '@oacore/design/src';
-// import textData from '../../data/membership.yml'
+import textData from '../../data/membership.yml'
 
-const DocumentationPageTemplate = () => (
-  // { headerAbout, docs }
+const DocumentationPageTemplate = ({ headerAbout, docs }) => {
+  const [highlight, setHighlight] = useState()
 
-  // const [highlight, setHighlight] = useState()
-  //
-  //   const mappedDocs = React.useMemo(() => {
-  //     return docs.items.map(item => ({
-  //       ...item,
-  //       description: item.descriptionAbout,
-  //     }))
-  //   }, [])
+  const mappedDocs = React.useMemo(
+    () =>
+      docs.items.map((item) => ({
+        ...item,
+        description: item.descriptionAbout,
+      })),
+    []
+  )
 
-  // temporary documentation until we get it from design system
-
-  <Layout>
-    {/* <DocumentationMembership */}
-    {/*  headerTitle={headerAbout.header.title} */}
-    {/*  headerCaption={<Markdown>{headerAbout.header.caption}</Markdown>} */}
-    {/*  docs={mappedDocs} */}
-    {/*  highlight={highlight} */}
-    {/*  setHighlight={setHighlight} */}
-    {/*  nav={<DocumentationMembershipNav
-     textData={textData}
-      setHighlight={setHighlight}></DocumentationMembershipNav>} */}
-    {/* > */}
-    {/* </DocumentationMembership> */}
-    <p>hi</p>
-    {/* remove temp comp */}
-    {/* <TempComp */}
-    {/*  headerTitle={headerAbout.header.title} */}
-    {/*  headerCaption={<Markdown>{headerAbout.header.caption}</Markdown>} */}
-    {/*  docs={mappedDocs} */}
-    {/*  highlight={highlight} */}
-    {/*  setHighlight={setHighlight} */}
-    {/*  nav={ */}
-    {/*    <DocumentationNav
-     textData={textData}
-      setHighlight={setHighlight}/> */}
-    {/*  } */}
-    {/* > */}
-    {/* </TempComp> */}
-  </Layout>
-)
+  return (
+    <Layout>
+      <DocumentationMembership
+        headerTitle={headerAbout.header.title}
+        headerCaption={<Markdown>{headerAbout.header.caption}</Markdown>}
+        docs={mappedDocs}
+        highlight={highlight}
+        setHighlight={setHighlight}
+        nav={
+          <DocumentationMembershipNav
+            textData={textData}
+            setHighlight={setHighlight}
+          />
+        }
+      />
+    </Layout>
+  )
+}
 
 export default DocumentationPageTemplate
