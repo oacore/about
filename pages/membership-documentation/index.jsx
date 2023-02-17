edcd3f3e2e72212883cfacd3c25fb09636417783
Valeriy Budko <valerij.budko@gmail.com>
2023-02-17 20:29:30 +0200

    Fix CORS (#835) (#836)

--- pages/membership-documentation/index.jsx
+++ pages/membership-documentation/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 import DocumentationPageTemplate from '../../templates/membership-documentation'
 
@@ -6,40 +6,44 @@ import retrieveContent from 'content'
 
 const ASSETS_BASE_URL = 'https://oacore.github.io/content/'
 
-const DocumentationPage = ({ ...props }) => {
-  const [stateData, setStateData] = useState({})
+const setAssetsUrl = (object) =>
+  Object.entries(object).forEach(([, value]) => {
+    delete value.membership
+    if (value.images) {
+      Object.entries(value.images).forEach(([, item]) => {
+        item.file = ASSETS_BASE_URL + item.file
+      })
+    }
+  })
 
-  const setAssetsUrl = (object) => {
-    Object.entries(object).forEach(([, value]) => {
-      delete value.membership
-      if (value.images) {
-        Object.entries(value.images).forEach(([, item]) => {
-          item.file = ASSETS_BASE_URL + item.file
-        })
-      }
-    })
-  }
+const getSections = async ({ ref } = {}) => {
+  const content = await retrieveContent('docs-membership', {
+    ref,
+    transform: 'object',
+  })
 
-  const getSections = async ({ ref } = {}) => {
-    const content = await retrieveContent('docs-membership', {
-      ref,
-      transform: 'object',
-    })
+  delete content.headerDashboard
+  Object.values(content).forEach((section) => {
+    if (section.items) setAssetsUrl(section.items)
+  })
 
-    delete content.headerDashboard
-    Object.values(content).forEach((section) => {
-      if (section.items) setAssetsUrl(section.items)
-    })
-
-    return content
-  }
-  if (Object.getOwnPropertyNames(stateData).length === 0) {
-    getSections().then((val) => {
-      setStateData(val)
-    })
-    return <></>
-  }
-  return <DocumentationPageTemplate {...props} {...stateData} />
+  return content
 }
+
+export async function getStaticProps({ previewData }) {
+  const ref = previewData?.ref
+  const sections = await getSections({ ref })
+  const data = {
+    ...sections,
+  }
+
+  return {
+    props: {
+      data,
+    },
+  }
+}
+
+const DocumentationPage = ({ data }) => <DocumentationPageTemplate {...data} />
 
 export default DocumentationPage
