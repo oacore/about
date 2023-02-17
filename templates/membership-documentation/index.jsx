78edc24ef67cc42bceec7e2df8d15f90887de5d5
ekachxaidze98 <65679299+ekachxaidze98@users.noreply.github.com>
2023-02-17 16:47:00 +0400

    Core 4461 membership documentation on about (#833)
    * update dataset text
    
    * update carousel
    
    * updates
    
    * component
    
    * update active state
    
    * update link issue
    
    * scroll issue to carousel
    
    * Add SCP police
    
    * update header text
    
    * update scroll issue from cards
    
    * Add SCP police
    
    * Remove SCP police
    
    * remove unused files
    
    * remove unused import
    
    ---------
    
    Co-authored-by: admin <valerij.budko@gmail.com>

---
+++ templates/membership-documentation/index.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react'
+import {
+  DocumentationMembership,
+  DocumentationMembershipNav,
+} from '@oacore/design/lib/modules'
+import { useRouter } from 'next/router'
+
+import Markdown from '../../components/markdown'
+import { Layout } from '../../design-v2/components'
+import textData from '../../data/membership.yml'
+
+function normalizeHref(str) {
+  const test = str.replace('#', '')
+  return test.replace('_', '-')
+}
+
+const DocumentationPageTemplate = ({ headerAbout, docs }) => {
+  const [highlight, setHighlight] = useState()
+  const [navActiveIndex, setNavActiveIndex] = useState(null)
+  const route = useRouter()
+  useEffect(() => {
+    const id = route.query?.r
+    if (id) {
+      setTimeout(() => {
+        document.getElementById(id).scrollIntoView({
+          behavior: 'smooth',
+          block: 'center',
+        })
+        const n = docs.items.findIndex((item) => item.id === id)
+        setHighlight(n)
+      }, 100)
+    }
+  }, [route.query.r])
+
+  useEffect(() => {
+    const id = route.query?.r
+    if (id) {
+      const n = textData.navItems.findIndex(
+        (item) => normalizeHref(item.href) === id
+      )
+      setNavActiveIndex(n)
+    }
+  }, [])
+  return (
+    <Layout>
+      <DocumentationMembership
+        headerTitle={headerAbout.header.title}
+        headerCaption={<Markdown>{headerAbout.header.caption}</Markdown>}
+        docs={docs}
+        highlight={highlight}
+        setHighlight={setHighlight}
+        nav={
+          <DocumentationMembershipNav
+            activeIndex={navActiveIndex}
+            setNavActiveIndex={setNavActiveIndex}
+            textData={textData}
+            setHighlight={setHighlight}
+          />
+        }
+      />
+    </Layout>
+  )
+}
+
+export default DocumentationPageTemplate
