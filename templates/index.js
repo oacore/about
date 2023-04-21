7674475abd2c41a034d6c2902aa533689da244d0
ekachxaidze98 <65679299+ekachxaidze98@users.noreply.github.com>
2023-04-21 15:54:03 +0400

    CORE-4524 advisory board (#901) (#902)

--- templates/index.js
+++ templates/index.js
@@ -1,4 +1,4 @@
 export { default as HistoryPage } from './history'
 export { default as MarkdownPage } from './markdown'
-export { default as ResearchPage } from './community/research'
+export { default as ResearchPage } from './governance/research'
 export { default as DocumentationPageOaiResolver } from './documentation'
