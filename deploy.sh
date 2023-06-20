f219cadaf8e4a414e5ae70df8e7adc5e7506b26a
andrew <anwiev@gmail.com>
2023-06-20 19:01:45 +0300

    added env prod before build

--- deploy.sh
+++ deploy.sh
@@ -122,7 +122,7 @@ echo "Running npm run build"
 echo "$DEPLOYMENT_TARGET"
 cd "$DEPLOYMENT_TARGET"
 eval node -v
-eval npm run build
+eval NODE_ENV=production npm run build
 exitWithMessageOnError "npm build failed"
 cd - > /dev/null
 
