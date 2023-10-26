a6eb3315ccec161b7009b23d86dc7fc988c082c3
ekachxaidze98 <65679299+ekachxaidze98@users.noreply.github.com>
2023-10-26 13:19:57 +0400

    Core 4513 update forms (#993) (#1016)
    * CORE-4513: update forms
    
    * add additional fields to form
    
    * temp textarea
    
    * render view by type
    
    * CORE-4513 add description textaerea
    
    * update dataset date
    
    * update small issues
    
    * CORE-4513: add dataset access
    
    * minor issues
    
    * update success message
    
    * update on forms
    
    * update core
    
    * update checkbox
    
    * add whats included

--- components/modal/registration/success.jsx
+++ components/modal/registration/success.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
-import { Modal, Button } from '@oacore/design/lib'
+import React, { useMemo } from 'react'
+import { Button, Modal } from '@oacore/design/lib'
+import { classNames } from '@oacore/design/lib/utils'
+import { useRouter } from 'next/router'
 
 import styles from './styles.module.scss'
 import emailsSVG from './images/Emails.svg'
@@ -7,26 +9,45 @@ import { patchStats } from '../../utils'
 import Markdown from '../../markdown'
 import findText from './helpers/findText'
 
-import { useStore, observe } from 'store'
+import { observe, useStore } from 'store'
 
-const ModalSuccess = observe(() => {
+const ModalSuccess = observe(({ responseData }) => {
   const { registration } = useStore()
+  const router = useRouter()
 
   const onCloseModal = () => {
     registration.reset()
   }
 
+  const contentToRender = useMemo(() => {
+    const isDatasetPath = router.pathname.includes('dataset')
+    const isSustainingMember = responseData?.memberStatus === 'sustaining'
+    const isSupportingMember = responseData?.memberStatus === 'supporting'
+
+    if (isDatasetPath) {
+      if (isSustainingMember) return findText('modalContent')
+      return findText('typeModalContent')
+    }
+    if (isSustainingMember || isSupportingMember)
+      return findText('modalContent')
+    return findText('typeModalContent')
+  }, [])
+
   return (
-    <Modal hideManually aria-label="success-modal" className={styles.modalSm}>
+    <Modal
+      hideManually
+      aria-label="success-modal"
+      className={classNames.use(styles.modalSm, {
+        [styles.modalBg]: registration.data.accountType === 'institution',
+      })}
+    >
       <h6>Success!</h6>
       <main className={styles.modalSmContent}>
         <div>
           <img src={emailsSVG} alt="emails" />
         </div>
 
-        <Markdown>
-          {patchStats(findText('modalContent'), registration.data)}
-        </Markdown>
+        <Markdown>{patchStats(contentToRender, registration.data)}</Markdown>
       </main>
       <footer className={styles.buttonGroup}>
         <Button variant="text" onClick={onCloseModal}>
