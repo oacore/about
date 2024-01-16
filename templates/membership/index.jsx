b6ea5f730647749d1ed235c44b816bdfe88dd37c
vbudko <valerij.budko@gmail.com>
2024-01-16 14:42:16 +0000

    Eslint

--- templates/membership/index.jsx
+++ templates/membership/index.jsx
@@ -244,7 +244,8 @@ const MembershipPageTemplate = ({ data, members }) => {
         <div className={styles.cardsWrapper}>
           {data.materials.cards.map((card) => (
             <article className={styles.materialsCard} key={card.image}>
-              {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+              {/* eslint-disable-next-line max-len */}
+              {/* eslint-disable-next-line jsx-a11y/anchor-is-valid,jsx-a11y/control-has-associated-label */}
               <a
                 target={card.action.target}
                 href={!card.video ? card.action.url : null}
