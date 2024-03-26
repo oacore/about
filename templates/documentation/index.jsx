1ca238702d9c5daea3b779f0364fe3bbe5658b1f
ekachxaidze98 <65679299+ekachxaidze98@users.noreply.github.com>
2024-03-26 15:46:58 +0400

    Core 4858 core video walkthrought (#1125)
    * CORE-4858: add video view
    
    * CORE-4858: add real urls
    
    * CORE-4858: update version
    
    * CORE-4858: add curson on tutorial
    
    * CORE-4858: env file
    
    * build issue

--- templates/documentation/index.jsx
+++ templates/documentation/index.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { Col, Row } from 'reactstrap'
 
 import faqData from '../../data/faq.yml'
 import styles from './documentation.module.scss'
 import Block from './components/block'
+import { Video } from '../../design-v2/components'
 
 import { Page, Section, Content, Markdown, Accordion } from 'components'
 
@@ -34,47 +35,74 @@ const faqDataAggregation = faqData.sections
     ['what-is-oai-base-url', 'oai-about', 'oai-not-resolve'].includes(elem.slug)
   )
 
-const DocumentationPageOaiResolver = ({ data }) => (
-  <Page
-    title={data.title}
-    description={data.description}
-    className={styles.page}
-  >
-    <Section className="section-wide">
-      <Row>
-        <Col md="12">
-          <img
-            src="/images/logo/OAI-resolver.png"
-            alt="OAI resolver"
-            width="170px"
-            height="82px"
-          />
-          <br />
-          <Content markdown>{data.body}</Content>
-        </Col>
-      </Row>
-    </Section>
+const DocumentationPageOaiResolver = ({ data }) => {
+  const [visibleVideo, setVisibleVideo] = useState(null)
 
-    <Block column1={data.block1Column1} column2={data.block1Column2} />
-    <Block column1={data.block2Column1} column2={data.block2Column2} />
-    <Block column1={data.block3Column1} column2={data.block3Column2} />
-    <Block column1={data.block4Column1} column2={data.block4Column2} />
+  const handleContentOpen = useCallback((condition) => {
+    if (condition) setVisibleVideo(condition)
+  }, [])
 
-    <Section className="section-wide">
-      <Row>
-        <Col md="12">
-          <h5>FAQs</h5>
-        </Col>
-      </Row>
-      <Row>
-        <Col md="12">
-          <br />
-          <FAQsSection items={faqDataAggregation} />
-          <br />
-        </Col>
-      </Row>
-    </Section>
-  </Page>
-)
+  const handleModalTrigger = (event) => {
+    const isModalTrigger =
+      event.target.getAttribute('data-modal-trigger') === 'true'
+    if (isModalTrigger) handleContentOpen(faqData.oaiUrl)
+  }
+
+  useEffect(() => {
+    document.addEventListener('click', handleModalTrigger)
+    return () => {
+      document.removeEventListener('click', handleModalTrigger)
+    }
+  }, [])
+
+  return (
+    <Page
+      title={data.title}
+      description={data.description}
+      className={styles.page}
+    >
+      <Section className="section-wide">
+        <Row>
+          <Col md="12">
+            <img
+              src="/images/logo/OAI-resolver.png"
+              alt="OAI resolver"
+              width="170px"
+              height="82px"
+            />
+            <br />
+            <Content markdown>{data.body}</Content>
+          </Col>
+        </Row>
+      </Section>
+
+      <Block column1={data.block1Column1} column2={data.block1Column2} />
+      <Block column1={data.block2Column1} column2={data.block2Column2} />
+      <Block column1={data.block3Column1} column2={data.block3Column2} />
+      <Block column1={data.block4Column1} column2={data.block4Column2} />
+      {visibleVideo && (
+        <Video
+          visibleModal={visibleVideo}
+          closeModal={() => setVisibleVideo(false)}
+          video={visibleVideo}
+        />
+      )}
+      <Section className="section-wide">
+        <Row>
+          <Col md="12">
+            <h5>FAQs</h5>
+          </Col>
+        </Row>
+        <Row>
+          <Col md="12">
+            <br />
+            <FAQsSection items={faqDataAggregation} />
+            <br />
+          </Col>
+        </Row>
+      </Section>
+    </Page>
+  )
+}
 
 export default DocumentationPageOaiResolver
