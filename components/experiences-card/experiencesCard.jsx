0d7dbb2c7da589a1eedf2759984effdcd3f30fbf
ekachxaidze98 <65679299+ekachxaidze98@users.noreply.github.com>
2024-04-04 17:45:27 +0400

    CORE-4884: add new use cases (#1136) (#1137)

---
+++ components/experiences-card/experiencesCard.jsx
@@ -0,0 +1,40 @@
+import { Button } from '@oacore/design/lib/elements'
+import React from 'react'
+
+import styles from './styles.module.scss'
+import { Markdown } from '../index'
+import Section from '../../design-v2/components/layout/section'
+
+const ExperiencesCard = ({ title, data }) => (
+  <Section id="member-experiences">
+    <div className={styles.institutionBenefitWrapper}>
+      <div className={styles.institutionBenefitInnerWrapper}>
+        <div className={styles.titleWrapper}>
+          <Markdown className={styles.mainTitle}>{title}</Markdown>
+        </div>
+        {data.map((repo) => (
+          <div className={styles.itemMainWrapper}>
+            <div className={styles.itemWrapper}>
+              <h5 className={styles.itemTitle}>{repo.title}</h5>
+              <p className={styles.itemDescription}>{repo.description}</p>
+            </div>
+            <div className={styles.footer}>
+              <div className={styles.userWrapper}>
+                <img className={styles.userImg} src={repo.img} alt="" />
+                <div>
+                  <h6 className={styles.user}>{repo.user}</h6>
+                  <span className={styles.position}>{repo.position}</span>
+                </div>
+              </div>
+              <Button target="blank" href={repo.action.url}>
+                {repo.action.title}
+              </Button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  </Section>
+)
+
+export default ExperiencesCard
