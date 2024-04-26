a7c979d2a7104cbbf1bf6e97d3277b93860aaa9c
ekachxaidze98 <65679299+ekachxaidze98@users.noreply.github.com>
2024-04-26 15:19:07 +0400

    CORE-4888: add advisory board on netlify (#1151)

--- pages/governance/advisory.jsx
+++ pages/governance/advisory.jsx
@@ -2,6 +2,44 @@ import React from 'react'
 
 import Advisory from '../../templates/governance/advisory'
 
-const AdvisoryPage = () => <Advisory />
+import retrieveContent from 'content'
+
+const ASSETS_BASE_URL = 'https://oacore.github.io/content/'
+
+const setAssetsUrl = (object) =>
+  Object.entries(object).forEach(([key, value]) => {
+    if (typeof value === 'string' && value.includes('/images'))
+      object[key] = ASSETS_BASE_URL + value
+  })
+
+const getSections = async ({ ref } = {}) => {
+  const content = await retrieveContent('advisory', {
+    ref,
+    transform: 'object',
+  })
+
+  Object.values(content).forEach((section) => {
+    setAssetsUrl(section)
+    if (section.box) setAssetsUrl(section.box)
+  })
+
+  return content
+}
+
+export async function getStaticProps({ previewData }) {
+  const ref = previewData?.ref
+  const sections = await getSections({ ref })
+  const data = {
+    ...sections,
+  }
+
+  return {
+    props: {
+      data,
+    },
+  }
+}
+
+const AdvisoryPage = ({ data }) => <Advisory {...data} />
 
 export default AdvisoryPage
