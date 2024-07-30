d2e64bacf4817a1d92372b4e45324d970cb8da18
ekachxaidze98 <65679299+ekachxaidze98@users.noreply.github.com>
2024-07-30 17:47:14 +0400

    CORE-4914: add service pages (#1193)

--- pages/membership/index.jsx
+++ pages/membership/index.jsx
@@ -4,16 +4,43 @@ import apiRequest from '../../api'
 
 import { Page } from 'components'
 import { countries } from 'data/countries.yml'
-import textData from 'data/membership.yml'
 import MembershipPageTemplate from 'templates/membership'
+import retrieveContent from 'content'
 
-const MembershipPage = ({ members }) => (
-  <Page title={textData.header.title} description={textData.header.description}>
-    <MembershipPageTemplate members={members} data={textData} />
+const ASSETS_BASE_URL = 'https://oacore.github.io/content/'
+
+const setAssetsUrl = (object) => {
+  Object.entries(object).forEach(([key, value]) => {
+    if (typeof value === 'string' && value.includes('/images'))
+      object[key] = ASSETS_BASE_URL + value
+    else if (typeof value === 'object') setAssetsUrl(value) // Recursively process nested objects
+  })
+}
+
+const getSections = async ({ ref } = {}) => {
+  const page = await retrieveContent('membership', {
+    ref,
+    transform: 'object',
+  })
+
+  setAssetsUrl(page)
+
+  return page
+}
+
+const MembershipPage = ({ members, page }) => (
+  <Page
+    title={page.header.header.title}
+    description={page.header.header.description}
+  >
+    <MembershipPageTemplate members={members} data={page} />
   </Page>
 )
 
-export async function getStaticProps() {
+export async function getStaticProps({ previewData }) {
+  const ref = previewData?.ref
+  const page = await getSections({ ref })
+
   const { data } = await apiRequest('/members')
   const members = data.map((item) => {
     if (item.country_code) {
@@ -26,7 +53,7 @@ export async function getStaticProps() {
     return item
   })
 
-  const filtered = members.filter(
+  const filteredMembers = members.filter(
     (member) =>
       member.billing_type !== 'starting' &&
       !member.organisation_name.toLowerCase().includes('test')
@@ -34,7 +61,8 @@ export async function getStaticProps() {
 
   return {
     props: {
-      members: filtered,
+      page,
+      members: filteredMembers,
     },
   }
 }
