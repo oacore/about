621f2932893cd7c14a3b3b5c14ace01562749534
ekachxaidze98 <ekachxaidze2@gmail.com>
2025-08-29 15:17:39 +0400

    CORE-5232: update api form

--- components/modal/registration/conditions.jsx
+++ components/modal/registration/conditions.jsx
@@ -35,20 +35,18 @@ const ModalConditions = observe(() => {
         labelText={<Markdown>{text.terms}</Markdown>}
         setCheckbox={setIsAgreeNewsletter}
       />
-      {registration.data.accountType !== 'personal' && (
-        <Checkbox
-          value
-          isDisabled
-          id="agreeNewsletter"
-          labelText={
-            router.pathname.includes('api')
-              ? ` I authorise CORE to send me information about the CORE API
+      <Checkbox
+        value
+        isDisabled
+        id="agreeNewsletter"
+        labelText={
+          router.pathname.includes('api')
+            ? ` I authorise CORE to send me information about the CORE API
               (required).`
-              : ` I authorise CORE to send me information about the CORE Dataset (required).`
-          }
-          setCheckbox={setIsAgreeNewsletter}
-        />
-      )}
+            : ` I authorise CORE to send me information about the CORE Dataset (required).`
+        }
+        setCheckbox={setIsAgreeNewsletter}
+      />
       <Checkbox
         id="agreeNewsletter"
         labelText={
