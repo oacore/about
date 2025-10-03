97efe88f46eb544f3e1311dccbdb146691e34e5e
ekachxaidze98 <65679299+ekachxaidze98@users.noreply.github.com>
2025-10-03 16:27:45 +0400

    CORE: add spin loader on search (#1386)

--- design-v2/components/search-form/index.jsx
+++ design-v2/components/search-form/index.jsx
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Select } from '@oacore/design/lib'
+import { Spinner } from 'reactstrap'
 
 import styles from './styles.module.scss'
 
@@ -9,6 +10,7 @@ import page from 'data/home.yml'
 
 const SearchForm = ({ setSearchValue, searchValue }) => {
   const router = useRouter()
+  const [isLoading, setIsLoading] = useState(false)
 
   const handleInput = (e) => {
     setSearchValue(e.value)
@@ -16,10 +18,12 @@ const SearchForm = ({ setSearchValue, searchValue }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault()
+    setIsLoading(true)
     router.push(`search?q=${searchValue}`)
   }
 
   const handleOnChange = () => {
+    setIsLoading(true)
     router.push(`search?q=${searchValue}`)
   }
   return (
@@ -34,6 +38,7 @@ const SearchForm = ({ setSearchValue, searchValue }) => {
         onInput={handleInput}
         className={styles.select}
         onChange={handleOnChange}
+        disabled={isLoading}
       >
         {/* {suggestions.map((el) => (
                   <Select.Option
@@ -46,8 +51,15 @@ const SearchForm = ({ setSearchValue, searchValue }) => {
                   </Select.Option>
                 ))} */}
       </Select>
-      <Button variant="contained" type="submit">
-        Search
+      <Button variant="contained" type="submit" disabled={isLoading}>
+        {isLoading ? (
+          <>
+            <Spinner color="#fff" className={styles.spinLoader} />
+            Searching...
+          </>
+        ) : (
+          'Search'
+        )}
       </Button>
     </form>
   )
