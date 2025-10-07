f56acc3660b67bd74b80606f9b39be92aa4c9537
ekachxaidze98 <65679299+ekachxaidze98@users.noreply.github.com>
2025-10-07 14:34:43 +0400

    CORE: fix page issues (#1388)
    * CORE: fix page issues
    
    * CORE: update terms

--- pages/terms/index.jsx
+++ pages/terms/index.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
 
-import textData from '../../data/terms.yml'
 import { Page } from '../../components'
 import TermsPageTemplate from '../../templates/term'
+import retrieveContent from '../../content'
 
-const TermsPage = () => (
-  <Page title={textData.header.title} description={textData.header.description}>
-    <TermsPageTemplate data={textData} />
+const getSections = async ({ ref } = {}) => {
+  const page = await retrieveContent('terms-conditions', {
+    ref,
+    transform: 'object',
+  })
+
+  return { page }
+}
+
+export async function getStaticProps({ previewData }) {
+  const ref = previewData?.ref
+  const { page } = await getSections({ ref })
+
+  return {
+    props: {
+      page,
+    },
+  }
+}
+
+const TermsPage = ({ page }) => (
+  <Page title={page.header.title} description={page.header.description}>
+    <TermsPageTemplate data={page} />
   </Page>
 )
 
--- templates/markdown.jsx
+++ templates/markdown.jsx
@@ -1,24 +1,35 @@
 import React from 'react'
 import { Container as BootstrapContainer } from 'reactstrap'
+import { useRouter } from 'next/router'
+
+import styles from './markdown.module.scss'
 
 import { Page, Content, Markdown } from 'components'
 
 // Since Page which is currently based on Article assumes that the content is
 // constructed from sections (with containers inside), we add a container to fix
 // the spacing
-const MarkdownPage = ({ data }) => (
-  <Page
-    title={data.title}
-    description={data.description}
-    keywords={data.keywords}
-  >
-    <h1>{data.headline || data.title}</h1>
-    <BootstrapContainer>
-      <Content>
-        <Markdown>{data.body}</Markdown>
-      </Content>
-    </BootstrapContainer>
-  </Page>
-)
+const MarkdownPage = ({ data }) => {
+  const router = useRouter()
+  const { slug } = router.query
+
+  const pageClass = styles[slug] || ''
+
+  return (
+    <Page
+      title={data.title}
+      description={data.description}
+      keywords={data.keywords}
+      className={pageClass}
+    >
+      <h1>{data.headline || data.title}</h1>
+      <BootstrapContainer>
+        <Content>
+          <Markdown>{data.body}</Markdown>
+        </Content>
+      </BootstrapContainer>
+    </Page>
+  )
+}
 
 export default MarkdownPage
