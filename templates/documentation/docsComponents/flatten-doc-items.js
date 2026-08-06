ba4bd226945e1d3c45c98107636f4f7fa515ac56
ekachxaidze98 <ekachxaidze2@gmail.com>
2026-08-06 14:17:30 +0400

    CORE: test new section naming

--- templates/documentation/docsComponents/flatten-doc-items.js
+++ templates/documentation/docsComponents/flatten-doc-items.js
@@ -1,6 +1,9 @@
 const flattenDocItems = (items = []) =>
   items
-    .flatMap((node) => [node, ...flattenDocItems(node.children ?? [])])
+    .flatMap((node) => [
+      node,
+      ...flattenDocItems(node.sections ?? node.children ?? []),
+    ])
     .filter(({ id }) => id)
     .map((node) => ({ ...node, id: node.id.replace(/^#/, '') }))
 
